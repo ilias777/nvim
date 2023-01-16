114d682928163b535aaac623712d2147279a26a8
Ilias <dougias.ilias@gmail.com>
2023-01-16 14:59:59 +0100

    Refactor init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,22 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ","
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
-require('core')
+-- require('core')
+require('core.options')
+require('core.keymaps')
+require('core.autocommands')
 
-require("lazy").setup('plugins')
+require('lazy').setup('plugins')
