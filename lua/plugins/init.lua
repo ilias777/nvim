f817174e89252c0170ce417335f1e9832e9f7b8d
Ilias <dougias.ilias@gmail.com>
2023-01-16 21:38:30 +0100

    Add settings to vimtex

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -244,6 +244,11 @@ return {
     {
         'lervag/vimtex',
         ft = 'tex',
+        config = function()
+            vim.g.vimtex_view_method = 'skim'
+            vim.g.vimtex_compiler_engine = 'lualatex'
+            vim.g.maplocalleader = ','
+        end,
     },
 
     -- COLORS
