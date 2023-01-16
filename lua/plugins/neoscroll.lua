64d100210e0905ca9808abccd35f9088e9eeda63
Ilias <dougias.ilias@gmail.com>
2023-01-16 20:07:05 +0100

    Add neoscroll plugin

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,20 @@
+return {
+    'karb94/neoscroll.nvim',
+    keys = {
+        { '<C-u>' },
+        { '<C-d>' },
+        { '<C-b>' },
+        { '<C-f>' },
+        { '<C-y>' },
+        { '<C-e>' },
+        { 'zt' },
+        { 'ze' },
+        { 'zb' },
+    },
+    config = function()
+        -- quadratic | cubic | quartic | quintic | circular | sine
+        require('neoscroll').setup({
+            easing_function = 'cubic',
+        })
+    end,
+}
