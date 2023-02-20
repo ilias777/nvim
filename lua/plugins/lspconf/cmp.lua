39635076c595c0debfd0120a9570e1a997b7be8d
Ilias <dougias.ilias@gmail.com>
2023-02-21 00:44:40 +0100

    Add again keymaps for luasnip & cmp (c-n / c-p)

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -70,6 +70,8 @@ cmp.setup({
         },
     },
     mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
