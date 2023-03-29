6e028ea0c9143f2be2ef24c17ff512c6695e1953
Ilias <dougias.ilias@gmail.com>
2023-03-29 14:43:08 +0200

    Add nvim-spider plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -225,6 +225,25 @@ return {
             require('sentiment').setup()
         end,
     },
+    {
+        'chrisgrieser/nvim-spider',
+        event = 'BufReadPost',
+        config = function()
+            -- Keymaps
+            vim.keymap.set({ 'n', 'o', 'x' }, 'w', function()
+                require('spider').motion('w')
+            end, { desc = 'Spider-w' })
+            vim.keymap.set({ 'n', 'o', 'x' }, 'e', function()
+                require('spider').motion('e')
+            end, { desc = 'Spider-e' })
+            vim.keymap.set({ 'n', 'o', 'x' }, 'b', function()
+                require('spider').motion('b')
+            end, { desc = 'Spider-b' })
+            vim.keymap.set({ 'n', 'o', 'x' }, 'ge', function()
+                require('spider').motion('ge')
+            end, { desc = 'Spider-ge' })
+        end,
+    },
 
     -- VIMTEX
     {
