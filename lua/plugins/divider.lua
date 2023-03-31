d833851d264c74dc49503e87a76cd3f5060ccb93
Ilias <dougias.ilias@gmail.com>
2023-03-31 11:42:03 +0200

    Add divider plugin for comments

--- lua/plugins/divider.lua
+++ lua/plugins/divider.lua
@@ -1,34 +1,61 @@
 return {
     'https://github.com/niuiic/divider.nvim',
-    enabled = false,
+    enabled = true,
     event = 'BufReadPre',
+    dependencies = {
+        'niuiic/niuiic-core.nvim',
+    },
     config = function()
         require('divider').setup({
             dividers = {
                 {
-                    -- === Test ===
-                    -- divider_pattern is passed to rg command
-                    -- divider_pattern = [[%%=+ [\s\S]+ =+%%]],
-                    divider_pattern = [[=== [\s\S]+ ===]],
-                    -- content_pattern is used by lua function string.match
-                    content_pattern = [[%%%%=+ ([%s%S]*) =+%%%%]],
+                    -- === test2 ===
+                    -- ==- test3 -==
+                    -- %%= hallo =%%
+                    -- %% test %%
+                    -- %%%%= hiii =%%%%
+                    -- %%- hi -%%
+                    -- %%%%%- hi %-%%%%
+                    -- regex used to match dividers
+                    -- this is passed to rg command
+                    divider_regex = [[%%=+ [\s\S]+ =+%%]],
+                    -- regex used to match content of each divider
+                    -- this is used by lua function string.match
+                    content_regex = [[%%%%=+ ([%s%S]*) =+%%%%]],
                     -- highlight color
                     hl = '#ff00ff',
-                    -- list = true means this divider will be listed in loclist
-                    list = true,
+                    -- icon (string | nil)
+                    icon = '',
+                    -- icon color (string | nil)
+                    icon_hl = '#ffff00',
+                    -- whether to show in list (boolean | nil)
+                    hide = false,
                 },
                 {
-                    divider_pattern = [[%%-+ [\s\S]+ -+%%]],
-                    content_pattern = [[%%%%%-+ ([%s%S]*) %-+%%%%]],
+                    divider_regex = [[%%-+ [\s\S]+ -+%%]],
+                    content_regex = [[%%%%%-+ ([%s%S]*) %-+%%%%]],
                     hl = '#ffff00',
-                    list = true,
+                    icon = '',
                 },
                 {
-                    divider_pattern = [[%% [\s\S]+ %%]],
-                    content_pattern = [[%%%% ([%s%S]*) %%%%]],
+                    divider_regex = [[%% [\s\S]+ %%]],
+                    content_regex = [[%%%% ([%s%S]*) %%%%]],
                     hl = '#00ff7c',
-                    list = false,
+                    icon = '',
                 },
+                {
+                    divider_regex = [[===+ [\s\S]+ =+==]],
+                    content_regex = [[==- ([%s%S]*) -==]],
+                    hl = '#ff00ff',
+                    icon = '',
+                },
+            },
+            enabled_filetypes = { 'lua' },
+            ui = {
+                -- "v" | "h"
+                direction = 'v',
+                size = 40,
+                enter = true,
             },
         })
     end,
