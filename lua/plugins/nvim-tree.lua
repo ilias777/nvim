f8c905c774e01a80f248ac4fad1faf3eccac1875
Ilias <dougias.ilias@gmail.com>
2023-04-04 00:34:51 +0200

    Add floating window option for nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -87,6 +87,18 @@ return {
                 --         { key = '<C-6>', action = 'cd' },
                 --     },
                 -- },
+                -- float = {
+                --     enable = true,
+                --     quit_on_focus_loss = true,
+                --     open_win_config = {
+                --         relative = 'editor',
+                --         border = 'rounded',
+                --         width = 40,
+                --         height = 30,
+                --         row = 1,
+                --         col = 120,
+                --     },
+                -- },
             },
             renderer = {
                 indent_markers = {
