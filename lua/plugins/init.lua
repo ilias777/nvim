379f534039ab26f7a2fc3e8afd693dcdd68ffeb6
Ilias <dougias.ilias@gmail.com>
2023-04-07 23:08:16 +0200

    Add statuscolumn plugin and set foldlevel=1

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -244,6 +244,21 @@ return {
             end, { desc = 'Spider-ge' })
         end,
     },
+    {
+        'luukvbaal/statuscol.nvim',
+        config = function()
+            local builtin = require('statuscol.builtin')
+            require('statuscol').setup({
+                relculright = true,
+                segments = {
+                    { sign = { name = { '.*' }, maxwidth = 1, colwidth = 1, auto = false }, click = 'v:lua.ScSa' },
+                    { sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
+                    { text = { builtin.lnumfunc, '  ' }, click = 'v:lua.ScLa' },
+                    { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+                },
+            })
+        end,
+    },
 
     -- === VIMTEX ===
     {
--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -19,7 +19,7 @@ return {
         'kevinhwang91/promise-async',
     },
     config = function()
-        vim.o.foldcolumn = '0'
+        vim.o.foldcolumn = '1'
         vim.o.foldlevel = 99
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
