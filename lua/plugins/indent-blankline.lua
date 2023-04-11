bad35b19f4a62da6a01baf890e395237f3523e59
Ilias <dougias.ilias@gmail.com>
2023-04-12 01:14:53 +0200

    Show current context in indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -11,8 +11,9 @@ return {
 
         require('indent_blankline').setup({
             space_char_blankline = ' ',
-            show_current_context = false,
-            show_current_context_start = false,
+            show_current_context = true,
+            show_current_context_start = true,
+            use_treesitter = true,
             filetype_exclude = {
                 'NvimTree',
                 'dashboard',
