780347c593f84e6381882c95f772bdbc6a4dfe3d
Ilias <dougias.ilias@gmail.com>
2023-04-15 20:44:22 +0200

    Disable line context for indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -11,8 +11,8 @@ return {
 
         require('indent_blankline').setup({
             space_char_blankline = ' ',
-            show_current_context = true,
-            show_current_context_start = true,
+            show_current_context = false,
+            show_current_context_start = false,
             use_treesitter = true,
             filetype_exclude = {
                 'NvimTree',
