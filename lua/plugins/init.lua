b5de2fc8007c1339c6e6559718e800301bdbc9a8
Ilias <dougias.ilias@gmail.com>
2023-04-23 00:59:17 +0200

    Adjust true zen padding for ataraxis mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -206,7 +206,18 @@ return {
             { '<leader>z', '<cmd>TZAtaraxis<cr>', desc = 'Zen Mode' },
         },
         config = function()
-            require('true-zen').setup()
+            require('true-zen').setup({
+                modes = {
+                    ataraxis = {
+                        padding = {
+                            left = 32,
+                            right = 52,
+                            top = 0,
+                            bottom = 0,
+                        },
+                    },
+                },
+            })
         end,
     },
     {
