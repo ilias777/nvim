39572f0e8447ea782b74423d4876f4ebcd239fff
Ilias <dougias.ilias@gmail.com>
2023-04-22 03:08:33 +0200

    Add ts_node_action to null-ls

--- lua/plugins/lspconf/null-ls.lua
+++ lua/plugins/lspconf/null-ls.lua
@@ -20,6 +20,16 @@ end
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
+-- Treesitter node actions
+require('null-ls').register({
+    name = 'more_actions',
+    method = { require('null-ls').methods.CODE_ACTION },
+    filetypes = { '_all' },
+    generator = {
+        fn = require('ts-node-action').available_actions,
+    },
+})
+
 -- Configuration
 require('null-ls').setup({
     debug = false,
