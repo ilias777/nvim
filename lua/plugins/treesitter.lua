21bb1afb52431cbf0b3d089a44469c460711c662
Ilias <dougias.ilias@gmail.com>
2023-06-09 22:07:43 +0200

    Format treesitter.lua and disable highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,10 +13,38 @@ return {
         },
         config = function()
             require('nvim-treesitter.configs').setup({
-                ensure_installed = { 'bash', 'bibtex', 'comment', 'cpp', 'css', 'help', 'html', 'http', 'java', 'javascript', 'jsdoc', 'json', 'json5', 'latex', 'lua', 'markdown', 'markdown_inline', 'make', 'php', 'python', 'regex', 'rust', 'scss', 'toml', 'vim', 'vue', 'yaml' },
+                ensure_installed = {
+                    'bash',
+                    'bibtex',
+                    'comment',
+                    'cpp',
+                    'css',
+                    'help',
+                    'html',
+                    'http',
+                    'java',
+                    'javascript',
+                    'jsdoc',
+                    'json',
+                    'json5',
+                    'latex',
+                    'lua',
+                    'markdown',
+                    'markdown_inline',
+                    'make',
+                    'php',
+                    'python',
+                    'regex',
+                    'rust',
+                    'scss',
+                    'toml',
+                    'vim',
+                    'vue',
+                    'yaml',
+                },
                 -- Buildin
                 highlight = {
-                    enable = true,
+                    enable = false,
                 },
                 -- Buildin
                 incremental_selection = {
