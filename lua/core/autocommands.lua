20979cbba035f333ca1ed7ef447463f5cab14f00
Ilias <dougias.ilias@gmail.com>
2023-06-22 19:50:46 +0200

    Deactivate inlay hints

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -6,23 +6,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         })
     end,
 })
---
--- vim.api.nvim_create_autocmd('BufWritePost', {
---     pattern = 'packer_init.lua',
---     callback = function ()
---         vim.cmd('luafile %')
---         vim.cmd('lua vim.notify("Packer Source", "info")')
---     end,
--- })
 
 -- Inlay hints
-vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
-    callback = function()
-        vim.lsp.buf.inlay_hint(0, true)
-    end,
-})
-vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
-    callback = function()
-        vim.lsp.buf.inlay_hint(0, false)
-    end,
-})
+-- vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+--     callback = function()
+--         vim.lsp.buf.inlay_hint(0, true)
+--     end,
+-- })
+-- vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+--     callback = function()
+--         vim.lsp.buf.inlay_hint(0, false)
+--     end,
+-- })
