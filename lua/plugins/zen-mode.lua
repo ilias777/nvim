eaaf65b1a3a649cd31162e01549d01c7d55ff85c
Ilias <dougias.ilias@gmail.com>
2023-06-24 19:08:32 +0200

    Add zen-mode plugin from folke

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+    'folke/zen-mode.nvim',
+    keys = {
+        { '<leader>z', '<cmd>ZenMode<cr>', desc = 'Zen Mode' },
+    },
+    opts = {
+        window = {
+            width = 100,
+            height = 0.7,
+        },
+    },
+}
