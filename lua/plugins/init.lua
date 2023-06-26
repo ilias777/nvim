23587568e5c5fc4fd6448a10bd60610affb62508
Ilias <dougias.ilias@gmail.com>
2023-06-26 21:51:52 +0200

    Remove pounce.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -274,7 +274,7 @@ return {
     },
     {
         'rlane/pounce.nvim',
-        enable = false,
+        enabled = false,
         keys = {
             { 'S', '<cmd>Pounce<CR>', mode = { 'n', 'v' } },
         },
