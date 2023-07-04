246c3228d78ddf143a20ada91781a63e5e40b7c3
Ilias <dougias.ilias@gmail.com>
2023-07-04 09:30:16 +0200

    Add multicursors.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -173,6 +173,18 @@ return {
         event = 'BufReadPost',
     },
     {
+        'smoka7/multicursors.nvim',
+        event = 'VeryLazy',
+        opts = {},
+        keys = {
+            {
+                '<Leader>m',
+                '<cmd>MCstart<cr>',
+                desc = 'Create a selection for word under the cursor',
+            },
+        },
+    },
+    {
         'famiu/bufdelete.nvim',
         enabled = false,
         cmd = 'Bdelete',
