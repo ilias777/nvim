dffdfb5937cdbb89793ab0b1aa9ca052677337bb
Ilias <dougias.ilias@gmail.com>
2023-07-08 00:01:24 +0200

    Add url for the usercommand

--- lua/core/usercommands.lua
+++ lua/core/usercommands.lua
@@ -1,4 +1,5 @@
 -- Open GitHub Repo from lazy.nvim
+-- url: https://github.com/justinsgithub/dotfiles/blob/main/neovim/.config/nvim/lua/user/commands.lua
 vim.api.nvim_create_user_command('OpenGithubRepo', function(_)
     local ghpath = vim.api.nvim_eval("shellescape(expand('<cfile>'))")
     local formatpath = ghpath:sub(2, #ghpath - 1)
