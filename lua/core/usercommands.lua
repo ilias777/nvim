0fd8876db971467c4cb00b1c7a34a0123c8bc3c3
Ilias <dougias.ilias@gmail.com>
2023-07-08 00:00:03 +0200

    Add usercommand for opening github repos

---
+++ lua/core/usercommands.lua
@@ -0,0 +1,10 @@
+-- Open GitHub Repo from lazy.nvim
+vim.api.nvim_create_user_command('OpenGithubRepo', function(_)
+    local ghpath = vim.api.nvim_eval("shellescape(expand('<cfile>'))")
+    local formatpath = ghpath:sub(2, #ghpath - 1)
+    local repourl = 'https://www.github.com/' .. formatpath
+    vim.fn.system({ 'open', repourl })
+end, {
+    desc = 'Open Github Repo',
+    force = true,
+})
