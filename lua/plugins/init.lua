68e173e6a7ecc1e384c18c10ba50e6a1ef4d12b4
Ilias <dougias.ilias@gmail.com>
2023-07-09 21:24:00 +0200

    Change multicursors.nvim configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -198,7 +198,23 @@ return {
         'smoka7/multicursors.nvim',
         -- enabled = true,
         event = 'VeryLazy',
-        opts = {},
+        dependencies = {
+            'smoka7/hydra.nvim',
+        },
+        opts = function()
+            local N = require('multicursors.normal_mode')
+            return {
+                normal_keys = {
+                    -- to change default lhs of key mapping change the key
+                    ['b'] = {
+                        -- assigning nil to method exits from multi cursor mode
+                        method = N.clear_others,
+                        -- description to show in hint window
+                        desc = 'Clear others',
+                    },
+                },
+            }
+        end,
         keys = {
             {
                 '<Leader>m',
