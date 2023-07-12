28191d615bb7e207bc36d9149151e9401cd7aa02
Ilias <dougias.ilias@gmail.com>
2023-07-12 12:52:37 +0200

    Add labels for keys in flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,14 +4,21 @@ return {
     ---@type Flash.Config
     opts = {
         label = {
-            rainbow = {
-                enabled = false,
-                shade = 4,
-            },
+            reuse = 'all', ---@type "lowercase" | "all"
         },
         modes = {
             char = {
+                jump_labels = true,
                 keys = { 'f', 'F', 't', 'T' },
+                char_actions = function(motion)
+                    return {
+                        [','] = 'next', -- set to `right` to always go right
+                        [';'] = 'prev', -- set to `left` to always go left
+                        -- clever-f style
+                        [motion:lower()] = 'next',
+                        [motion:upper()] = 'prev',
+                    }
+                end,
             },
         },
     },
