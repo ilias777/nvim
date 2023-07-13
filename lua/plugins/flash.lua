67bf086c018c6101b7c6a125f483562bd2313b22
Ilias <dougias.ilias@gmail.com>
2023-07-13 09:28:32 +0200

    Cleanup flash.nvim configuration

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -12,9 +12,8 @@ return {
                 keys = { 'f', 'F', 't', 'T' },
                 char_actions = function(motion)
                     return {
-                        [','] = 'next', -- set to `right` to always go right
-                        [';'] = 'prev', -- set to `left` to always go left
-                        -- clever-f style
+                        [','] = 'next',
+                        [';'] = 'prev',
                         [motion:lower()] = 'next',
                         [motion:upper()] = 'prev',
                     }
@@ -27,7 +26,6 @@ return {
             's',
             mode = { 'n', 'x', 'o' },
             function()
-                -- default options: exact mode, multi window, all directions, with a backdrop
                 require('flash').jump()
             end,
         },
@@ -42,7 +40,6 @@ return {
             'r',
             mode = 'o',
             function()
-                -- jump to a remote location to execute the operator
                 require('flash').remote()
             end,
             desc = 'Remote Flash',
@@ -51,7 +48,6 @@ return {
             '<Space>v',
             mode = { 'n', 'o', 'x' },
             function()
-                -- show labeled treesitter nodes around the search matches
                 require('flash').treesitter_search()
             end,
             desc = 'Treesitter Search',
