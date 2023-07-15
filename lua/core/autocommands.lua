e028fbebd8186c7bc6ff3203f20b70295ea3771f
Ilias <dougias.ilias@gmail.com>
2023-07-15 23:25:26 +0200

    Add some autocommands

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,9 +1,23 @@
+-- Highlight selectes text
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank({
             higroup = 'IncSearch',
             timeout = 100,
         })
+    end,
+})
+
+-- Cursorline on normal mode and no cursorline on insert mode
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+    callback = function()
+        vim.opt.cursorline = true
+    end,
+})
+
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+    callback = function()
+        vim.opt.cursorline = false
     end,
 })
 
