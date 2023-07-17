64f7b8a0e3f9f738be029483c58d1c5d132ca48a
Ilias <dougias.ilias@gmail.com>
2023-07-17 19:21:02 +0200

    Update lspconfig configuration to the new config

--- lua/plugins/lspconf/lsp-config.lua
+++ lua/plugins/lspconf/lsp-config.lua
@@ -59,55 +59,88 @@ vim.keymap.set('n', '<space><left>', vim.diagnostic.goto_prev, opts('Previous Di
 vim.keymap.set('n', '<space><right>', vim.diagnostic.goto_next, opts('Next Diagnostic'))
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts('Send Diagnostic to Locallist'))
 
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = function(desc)
-        return { noremap = true, silent = true, buffer = bufnr, desc = desc }
-    end
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts('Go to Declaration'))
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts('Go to Definition'))
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts('Hover'))
-    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts('Go to Implementation'))
-    vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts('Signature Help'))
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts('Add workspace folder'))
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts('Remove workspace folder'))
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts('List workspace folders'))
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts('Type Definition'))
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts('Rename LSP'))
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts('Code Action LSP'))
-    vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts('Go to Reference'))
-    vim.keymap.set('n', '<space>f', function()
-        vim.lsp.buf.format({ async = true })
-    end, bufopts('Formatting'))
-    vim.keymap.set('v', '<space>1f', vim.lsp.buf.format, bufopts('Range Fromatting'))
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format({ async = true })
+        end, bufopts)
+    end,
+})
 
-    -- Winbar with Navic
-    -- navic.attach(client, bufnr)
+-- local on_attach = function(client, bufnr)
+--     -- Enable completion triggered by <c-x><c-o>
+--     -- vim.api.nvim_buf_set_option() is deprecated. New function is vim.api.nvim_set_option_value()()
+--     -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--
+--     -- Mappings.
+--     -- See `:help vim.lsp.*` for documentation on any of the below functions
+--     local bufopts = function(desc)
+--         return { noremap = true, silent = true, buffer = bufnr, desc = desc }
+--     end
+--     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts('Go to Declaration'))
+--     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts('Go to Definition'))
+--     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts('Hover'))
+--     vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts('Go to Implementation'))
+--     vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts('Signature Help'))
+--     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts('Add workspace folder'))
+--     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts('Remove workspace folder'))
+--     vim.keymap.set('n', '<space>wl', function()
+--         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--     end, bufopts('List workspace folders'))
+--     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts('Type Definition'))
+--     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts('Rename LSP'))
+--     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts('Code Action LSP'))
+--     vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts('Go to Reference'))
+--     vim.keymap.set('n', '<space>f', function()
+--         vim.lsp.buf.format({ async = true })
+--     end, bufopts('Formatting'))
+--     vim.keymap.set('v', '<space>1f', vim.lsp.buf.format, bufopts('Range Fromatting'))
+--
+--     -- Winbar with Navic
+--     -- navic.attach(client, bufnr)
+--
+--     -- Inlay hints
+--     if client.server_capabilities.inlayHintProvider then
+--         vim.lsp.buf.inlay_hint(bufnr, true)
+--     end
+--
+--     -- Inlay hints only on insert mode
+--     -- vim.lsp.buf.inlay_hint(bufnr, true)
+--     -- vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+--     --     callback = function()
+--     --         vim.lsp.buf.inlay_hint(bufnr, true)
+--     --     end,
+--     -- })
+--     -- vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+--     --     callback = function()
+--     --         vim.lsp.buf.inlay_hint(bufnr, false)
+--     --     end,
+--     -- })
+-- end
 
-    -- Inlay hints
-    if client.server_capabilities.inlayHintProvider then
-        vim.lsp.buf.inlay_hint(bufnr, true)
-    end
-
-    -- Inlay hints only on insert mode
-    -- vim.lsp.buf.inlay_hint(bufnr, true)
-    -- vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
-    --     callback = function()
-    --         vim.lsp.buf.inlay_hint(bufnr, true)
-    --     end,
-    -- })
-    -- vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
-    --     callback = function()
-    --         vim.lsp.buf.inlay_hint(bufnr, false)
-    --     end,
-    -- })
-end
+-- Inlay Hints
+vim.lsp.inlay_hint(0, true)
 
 -- Borders
 -- vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#181926]])
@@ -158,7 +191,7 @@ vim.diagnostic.config({
 -- Lua server
 lspconfig.lua_ls.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     settings = {
         Lua = {
@@ -206,7 +239,7 @@ lspconfig.lua_ls.setup({
 -- JavaScript Server
 lspconfig.tsserver.setup({
     capabilities = capabilities,
-    -- on_attach = on_attach,
+    -- on_attach = on_attach
     handlers = handlers,
     init_options = {
         preferences = {
@@ -244,14 +277,14 @@ lspconfig.tsserver.setup({
 -- Python Server
 lspconfig.pyright.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
 })
 
 -- Emmet Server
 lspconfig.emmet_ls.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     filetypes = { 'astro', 'css', 'eruby', 'html', 'htmldjango', 'javascript', 'javascriptreact', 'less', 'pug', 'sass', 'scss', 'svelte', 'typescriptreact', 'vue' },
 })
@@ -259,7 +292,7 @@ lspconfig.emmet_ls.setup({
 -- CSS Server
 lspconfig.cssls.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     settings = {
         css = {
@@ -273,14 +306,14 @@ lspconfig.cssls.setup({
 -- Tailwind Server
 -- lspconfig.tailwindcss.setup({
 --     capabilities = capabilities,
---     on_attach = on_attach,
+--     -- on_attach = on_attach,
 --     handlers = handlers,
 -- })
 
 -- Volar Vue Server
 lspconfig.volar.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     filetypes = {
         'vue', --[[ 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'json' ]]
@@ -313,7 +346,7 @@ lspconfig.volar.setup({
 -- JSON Server
 lspconfig.jsonls.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     filetypes = { 'json', 'jsonc' },
     init_options = {
@@ -324,14 +357,14 @@ lspconfig.jsonls.setup({
 -- HTML Server
 lspconfig.html.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
 })
 
 -- LTex Server
 lspconfig.ltex.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     settings = {
         ltex = {
@@ -343,7 +376,7 @@ lspconfig.ltex.setup({
 -- TexLab Server
 lspconfig.texlab.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     settings = {
         texlab = {
@@ -375,21 +408,21 @@ lspconfig.texlab.setup({
 -- PHP Server
 lspconfig.intelephense.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
 })
 
 -- Java Server
 -- lspconfig.jdtls.setup({
 --     capabilities = capabilities,
---     on_attach = on_attach,
+--     -- on_attach = on_attach,
 --     handlers = handlers,
 -- })
 
 -- YAML Server
 lspconfig.yamlls.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
     settings = {
         yaml = {
@@ -417,6 +450,6 @@ lspconfig.yamlls.setup({
 -- Rust
 lspconfig.rust_analyzer.setup({
     capabilities = capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     handlers = handlers,
 })
