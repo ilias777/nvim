07267c1d51bf14d30be221151838c6e71cb94866
Ilias <dougias.ilias@gmail.com>
2023-07-17 19:40:08 +0200

    Update nvim-autopairs to add parentheses with cmp

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -45,6 +45,7 @@ local luasnip = require('luasnip')
 
 -- Setup nvim-cmp
 local cmp = require('cmp')
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -162,4 +163,31 @@ cmp.setup.cmdline(':', {
 
 -- Autopairs
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+local ts_utils = require('nvim-treesitter.ts_utils')
+
+local ts_node_func_parens_disabled = {
+    -- ecma
+    named_imports = true,
+    -- rust
+    use_declaration = true,
+}
+
+local default_handler = cmp_autopairs.filetypes['*']['('].handler
+cmp_autopairs.filetypes['*']['('].handler = function(char, item, bufnr, rules, commit_character)
+    local node_type = ts_utils.get_node_at_cursor():type()
+    if ts_node_func_parens_disabled[node_type] then
+        if item.data then
+            item.data.funcParensDisabled = true
+        else
+            char = ''
+        end
+    end
+    default_handler(char, item, bufnr, rules, commit_character)
+end
+
+cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done({
+        sh = false,
+    })
+)
