1a5450a8a7703a907135598f1428d2783d71ab72
Ilias <dougias.ilias@gmail.com>
2023-07-27 14:54:15 +0300

    Move keybindings from which-key.lua to keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,7 @@
 --  ╭──────────────────────────────────────────────────────────╮
 --  │                      BASIC KEYMAPS                       │
 --  ╰──────────────────────────────────────────────────────────╯
+
 -- Exit from insert mode
 vim.keymap.set('i', 'kj', '<esc>')
 
@@ -86,12 +87,64 @@ vim.keymap.set('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
 -- Bufferline.nvim
 vim.keymap.set('n', '<S-Left>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Buffer Previous' })
 vim.keymap.set('n', '<S-Right>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Buffer Next' })
+vim.keymap.set('n', '<space>bp', '<cmd>BufferLineTogglePin<cr>', { desc = 'Pin Buffer' })
+vim.keymap.set('n', '<space>bk', '<cmd>BufferLinePick<cr>', { desc = 'Pick Buffer' })
+vim.keymap.set('n', '<space>b<left>', '<cmd>BufferLineMovePrev<cr>', { desc = 'Move Buffer to the left' })
+vim.keymap.set('n', '<space>b<right>', '<cmd>BufferLineMoveNext<cr>', { desc = 'Move Buffer to the right' })
+vim.keymap.set('n', '<space>b1', '<cmd>BufferLineGoToBuffer 1<cr>', { desc = 'Go to Buffer 1' })
+vim.keymap.set('n', '<space>b2', '<cmd>BufferLineGoToBuffer 2<cr>', { desc = 'Go to Buffer 2' })
+vim.keymap.set('n', '<space>b3', '<cmd>BufferLineGoToBuffer 3<cr>', { desc = 'Go to Buffer 3' })
+vim.keymap.set('n', '<space>b4', '<cmd>BufferLineGoToBuffer 4<cr>', { desc = 'Go to Buffer 4' })
+vim.keymap.set('n', '<space>b5', '<cmd>BufferLineGoToBuffer 5<cr>', { desc = 'Go to Buffer 5' })
+vim.keymap.set('n', '<space>b6', '<cmd>BufferLineGoToBuffer 6<cr>', { desc = 'Go to Buffer 6' })
+vim.keymap.set('n', '<space>b7', '<cmd>BufferLineGoToBuffer 7<cr>', { desc = 'Go to Buffer 7' })
+vim.keymap.set('n', '<space>b8', '<cmd>BufferLineGoToBuffer 8<cr>', { desc = 'Go to Buffer 8' })
+vim.keymap.set('n', '<space>b9', '<cmd>BufferLineGoToBuffer 9<cr>', { desc = 'Go to Buffer 9' })
 
--- Treehopper
+-- nvim-treehopper
 vim.keymap.set('o', 'm', [[:<c-u>lua require 'tsht'.nodes()<cr>]], { silent = true, remap = true })
 vim.keymap.set('x', 'm', [[:<c-u>lua require 'tsht'.nodes()<cr>]], { silent = true })
 
--- Lazy.nvim
+-- lazy.nvim
 vim.keymap.set('n', '<Leader>la', '<cmd>Lazy<cr>', { desc = 'Open Lazy' })
 vim.keymap.set('n', '<Leader>lc', '<cmd>Lazy check<cr>', { desc = 'Check Lazy Plugins' })
 vim.keymap.set('n', '<Leader>ls', '<cmd>Lazy sync<cr>', { desc = 'Sync Lazy Plugins' })
+
+-- nvim-dap
+vim.keymap.set('n', '<leader>dB', '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { desc = 'Add Conditional Breakpoint' })
+vim.keymap.set('n', '<leader>dc', '<cmd>DapContinue<CR>', { desc = 'Dap Continue' })
+vim.keymap.set('n', '<leader>di', '<cmd>DapStepInto<CR>', { desc = 'Dap Step Into' })
+vim.keymap.set('n', '<leader>do', '<cmd>DapStepOver<CR>', { desc = 'Dap Step Over' })
+vim.keymap.set('n', '<leader>ds', '<cmd>DapStepOut<CR>', { desc = 'Dap Step Out' })
+vim.keymap.set('n', '<leader>dt', '<cmd>DapTerminate<CR>', { desc = 'Dap Terminate' })
+vim.keymap.set('n', '<leader>dl', '<cmd>DapShowLog<CR>', { desc = 'Dap Show Log' })
+vim.keymap.set('n', '<leader>dr', '<cmd>DapToggleRepl<CR>', { desc = 'Dap Toggle Repl' })
+
+-- telescope.nvim
+vim.keymap.set('n', '<leader>fc', ":lua require'telescope.builtin'.commands{}<cr>", { desc = 'List Commands' })
+vim.keymap.set('n', '<leader>fj', '<cmd>Telescope emoji<cr>', { desc = 'Find emoji' })
+vim.keymap.set('n', '<leader>fo', '<cmd>Telescope oldfiles<cr>', { desc = 'Recently opened files' })
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope symbols<cr>', { desc = 'Find Symbols' })
+vim.keymap.set('n', '<leader>fr', ':lua require"telescope.builtin".registers{}<cr>', { desc = 'List Registers' })
+
+-- diffview.nvim
+vim.keymap.set('n', '<leader>go', '<cmd>DiffviewOpen<cr>', { desc = 'Open Diffview' })
+vim.keymap.set('n', '<leader>gc', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' })
+
+-- hop.nvim
+vim.keymap.set('n', '<leader>hl', '<cmd>HopLine<cr>', { desc = 'Hop Line' })
+vim.keymap.set('n', '<leader>h1', '<cmd>HopChar1<cr>', { desc = 'Hop 1 Char' })
+vim.keymap.set('n', '<leader>h2', '<cmd>HopChar2<cr>', { desc = 'Hop 2 Chars' })
+vim.keymap.set('n', '<leader>hc', '<cmd>HopVertical<cr>', { desc = 'Hop Vertical' })
+
+-- trouble.nvim
+vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', { desc = 'Trouble Workspace Diagnostics' })
+vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle lsp_document_diagnostics<cr>', { desc = 'Trouble Document Diagnostics' })
+vim.keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', { desc = 'Trouble Location List' })
+vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', { desc = 'Trouble Quickfix' })
+
+-- noice.nvim
+vim.keymap.set('n', '<space>nn', '<cmd>Noice<cr>', { desc = 'Noice Messages' })
+vim.keymap.set('n', '<space>nd', '<cmd>NoiceDismiss<cr>', { desc = 'Noice Dismiss' })
+vim.keymap.set('n', '<space>nl', '<cmd>NoiceLast<cr>', { desc = 'Noice Last' })
+vim.keymap.set('n', '<space>nt', '<cmd>NoiceTelescope<cr>', { desc = 'Noice Telescope' })
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,127 +5,33 @@ return {
     init = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 500
-    end,
-    config = function()
-        require('which-key').setup({
-            window = {
-                border = 'rounded', -- none, single, double, shadow
-                position = 'bottom', -- bottom, top
-                margin = { 0, 10, 1, 10 }, -- extra window margin [top, right, bottom, left]
-            },
-            layout = {
-                height = { min = 4, max = 15 }, -- min and max height of the columns
-                width = { min = 20, max = 50 }, -- min and max width of the columns
-                spacing = 2, -- spacing between columns
-                align = 'center', -- align columns left, center or right
-            },
-        })
-
         local wk = require('which-key')
         wk.register({
-            --  ╭──────────────────────────────────────────────────────────╮
-            --  │                        LEADER KEY                        │
-            --  ╰──────────────────────────────────────────────────────────╯
-
-            -- Comment Frame & Comment Box
-            ['<leader>c'] = { name = '+Comment Frame/Box' },
-
-            -- Dap
-            ['<leader>d'] = { name = '+Debug' },
-            ['<leader>dB'] = { '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', 'Add Conditional Breakpoint' },
-            ['<leader>dc'] = { '<cmd>DapContinue<CR>', 'Dap Continue' },
-            ['<leader>di'] = { '<cmd>DapStepInto<CR>', 'Dap Step Into' },
-            ['<leader>do'] = { '<cmd>DapStepOver<CR>', 'Dap Step Over' },
-            ['<leader>ds'] = { '<cmd>DapStepOut<CR>', 'Dap Step Out' },
-            ['<leader>dt'] = { '<cmd>DapTerminate<CR>', 'Dap Terminate' },
-            ['<leader>dl'] = { '<cmd>DapShowLog<CR>', 'Dap Show Log' },
-            ['<leader>dr'] = { '<cmd>DapToggleRepl<CR>', 'Dap Toggle Repl' },
-            -- ['<leader>db'] = { '<cmd>DapToggleBreakpoint<CR>', 'Add Breakpoint' },
-            -- ['<leader>du'] = { '<cmd>lua require("dapui").toggle()<CR>', 'Dap UI Toggle' },
-
-            -- Telescope
-            ['<leader>f'] = { name = '+File' },
-            ['<leader>fc'] = { ":lua require'telescope.builtin'.commands{}<cr>", 'List Commands' },
-            ['<leader>fj'] = { '<cmd>Telescope emoji<cr>', 'Find emoji' },
-            ['<leader>fo'] = { '<cmd>Telescope oldfiles<cr>', 'Recently opened files' },
-            ['<leader>fs'] = { '<cmd>Telescope symbols<cr>', 'Find Symbols' },
-            ['<leader>fr'] = { ':lua require"telescope.builtin".registers{}<cr>', 'List Registers' },
-            -- ['<leader>fk'] = { '<cmd>Telescope keymaps<cr>', 'Find keymap' },
-            -- ['<leader>fb'] = { '<cmd>Telescope buffers<cr>', 'Find Buffers' },
-            -- ['<leader>fe'] = { '<cmd>Telescope file_browser<cr>', 'Browse Files' },
-            -- ['<leader>ff'] = { '<cmd>Telescope find_files<cr>', 'Find File' },
-            -- ['<leader>fg'] = { '<cmd>Telescope live_grep<cr>', 'Find Word' },
-            -- ['<leader>fh'] = { '<cmd>Telescope help_tags<cr>', 'Find Help' },
-
-            -- Git
-            ['<leader>g'] = { name = '+Git' },
-            ['<leader>go'] = { '<cmd>DiffviewOpen<cr>', 'Open Diffview' },
-            ['<leader>gc'] = { '<cmd>DiffviewClose<cr>', 'Close Diffview' },
-            -- ['<leader>gg'] = { '<cmd>Neogit<cr>', 'Neogit' },
-
-            -- Hop
-            ['<leader>h'] = { name = 'Hunk Gitsigns/Hop Motion' },
-            ['<leader>hl'] = { '<cmd>HopLine<cr>', 'Hop Line' },
-            ['<leader>h1'] = { '<cmd>HopChar1<cr>', 'Hop 1 Char' },
-            ['<leader>h2'] = { '<cmd>HopChar2<cr>', 'Hop 2 Chars' },
-            ['<leader>hc'] = { '<cmd>HopVertical<cr>', 'Hop Vertical' },
-            -- ['<leader>hp'] = { '<cmd>HopPattern<cr>', 'Hop Pattern' },
-            -- ['<leader>ha'] = { '<cmd>HopAnywhere<cr>', 'Hop Anywhere' },
-            -- ['<leader>a'] = { '<cmd>HopWord<cr>', 'Hop Word' },
-
-            -- Gitsigns
-            ['<leader>t'] = { name = '+Toggle' },
-
-            -- Symbols Outline
-            -- ['<leader>o'] = { '<cmd>SymbolsOutline<cr>', 'Symbols Outline Toggle' },
-
-            -- Trouble
-            ['<leader>x'] = { name = '+Trouble' },
-            ['<leader>xw'] = { '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', 'Trouble Workspace Diagnostics' },
-            ['<leader>xd'] = { '<cmd>TroubleToggle lsp_document_diagnostics<cr>', 'Trouble Document Diagnostics' },
-            ['<leader>xl'] = { '<cmd>TroubleToggle loclist<cr>', 'Trouble Location List' },
-            ['<leader>xq'] = { '<cmd>TroubleToggle quickfix<cr>', 'Trouble Quickfix' },
-            -- ['<leader>xx'] = { '<cmd>TroubleToggle<cr>', 'Trouble Toggle' },
-
-            --  ╭──────────────────────────────────────────────────────────╮
-            --  │                        SPACE KEY                         │
-            --  ╰──────────────────────────────────────────────────────────╯
-
-            -- Bufferline
-            ['<space>b'] = { name = '+Buffer' },
-            ['<space>bp'] = { '<cmd>BufferLineTogglePin<cr>', 'Pin Buffer' },
-            ['<space>bk'] = { '<cmd>BufferLinePick<cr>', 'Pick Buffer' },
-            ['<space>b<left>'] = { '<cmd>BufferLineMovePrev<cr>', 'Move Buffer to the left' },
-            ['<space>b<right>'] = { '<cmd>BufferLineMoveNext<cr>', 'Move Buffer to the right' },
-            ['<space>b1'] = { '<cmd>BufferLineGoToBuffer 1<cr>', 'Go to Buffer 1' },
-            ['<space>b2'] = { '<cmd>BufferLineGoToBuffer 2<cr>', 'Go to Buffer 2' },
-            ['<space>b3'] = { '<cmd>BufferLineGoToBuffer 3<cr>', 'Go to Buffer 3' },
-            ['<space>b4'] = { '<cmd>BufferLineGoToBuffer 4<cr>', 'Go to Buffer 4' },
-            ['<space>b5'] = { '<cmd>BufferLineGoToBuffer 5<cr>', 'Go to Buffer 5' },
-            ['<space>b6'] = { '<cmd>BufferLineGoToBuffer 6<cr>', 'Go to Buffer 6' },
-            ['<space>b7'] = { '<cmd>BufferLineGoToBuffer 7<cr>', 'Go to Buffer 7' },
-            ['<space>b8'] = { '<cmd>BufferLineGoToBuffer 8<cr>', 'Go to Buffer 8' },
-            ['<space>b9'] = { '<cmd>BufferLineGoToBuffer 9<cr>', 'Go to Buffer 9' },
-            -- ['<space>bd'] = { '<cmd>Bdelete<cr>', 'Delete Buffer' },
-
-            -- Code Actions with LSP
-            ['<space>c'] = { name = '+Code actions / Cheatsheet' },
-
-            -- Messages
-            -- ['<leader>m'] = { '<cmd>messages<cr>', 'Show Messages' },
-
-            -- Noice
-            ['<space>n'] = { name = '+Noice' },
-            ['<space>nn'] = { '<cmd>Noice<cr>', 'Noice Messages' },
-            ['<space>nd'] = { '<cmd>NoiceDismiss<cr>', 'Noice Dismiss' },
-            ['<space>nl'] = { '<cmd>NoiceLast<cr>', 'Noice Last' },
-            ['<space>nt'] = { '<cmd>NoiceTelescope<cr>', 'Noice Telescope' },
-
-            -- Workspace LSP
-            ['<space>w'] = { name = '+Workspace LSP' },
-
-            -- Rename with LSP
-            ['<space>r'] = { name = '+Rename with LSP' },
+            ['<leader>c'] = { name = '+Comment Frame/Box' }, -- Comment Frame & Comment Box
+            ['<leader>d'] = { name = '+Debug' }, -- nvim-dap
+            ['<leader>f'] = { name = '+File' }, -- Telescope
+            ['<leader>g'] = { name = '+Git' }, -- Git
+            ['<leader>h'] = { name = 'Hunk Gitsigns/Hop Motion' }, -- Hop
+            ['<leader>t'] = { name = '+Toggle' }, -- Gitsigns
+            ['<leader>x'] = { name = '+Trouble' }, -- Trouble
+            ['<space>b'] = { name = '+Buffer' }, -- Bufferline.nvim
+            ['<space>c'] = { name = '+Code actions / Cheatsheet' }, -- Code Actions with LSP
+            ['<space>n'] = { name = '+Noice' }, -- Noice.nvim
+            ['<space>w'] = { name = '+Workspace LSP' }, -- Workspace LSP
+            ['<space>r'] = { name = '+Rename with LSP' }, -- Rename with LSP
         })
     end,
+    opts = {
+        window = {
+            border = 'rounded', -- none, single, double, shadow
+            position = 'bottom', -- bottom, top
+            margin = { 0, 10, 1, 10 }, -- extra window margin [top, right, bottom, left]
+        },
+        layout = {
+            height = { min = 4, max = 15 }, -- min and max height of the columns
+            width = { min = 20, max = 50 }, -- min and max width of the columns
+            spacing = 2, -- spacing between columns
+            align = 'center', -- align columns left, center or right
+        },
+    },
 }
