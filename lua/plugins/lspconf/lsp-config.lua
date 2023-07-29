0e5e9b408eaa0df0299136f98579baf0c6cd98f8
Ilias <dougias.ilias@gmail.com>
2023-07-29 11:00:29 +0300

    Add description to lsp keybindings

--- lua/plugins/lspconf/lsp-config.lua
+++ lua/plugins/lspconf/lsp-config.lua
@@ -67,24 +67,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        local bufopts = function(desc)
+            return { buffer = ev.buf, desc = desc }
+        end
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts('Go to Declaration'))
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts('Go to Definition'))
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts('Hover'))
+        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts('Go to Implementation'))
+        vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts('Singature Help'))
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts('Add Workspace Folder'))
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts('Remive Workspace Folder'))
         vim.keymap.set('n', '<space>wl', function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts)
+        end, bufopts('List Workspace Folder'))
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts('Type Definition'))
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts('Rename with LSP'))
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts('Code Action'))
+        vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts('Go to Reference'))
         vim.keymap.set('n', '<space>f', function()
             vim.lsp.buf.format({ async = true })
-        end, bufopts)
+        end, bufopts('Formatting with LSP'))
 
         -- Inlay Hints
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
