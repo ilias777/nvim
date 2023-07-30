0e34ad207a88a31d324e7a8335a5db8e26e43ffc
Ilias <dougias.ilias@gmail.com>
2023-07-30 11:37:05 +0300

    Change keybingings for gitsigns.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -39,9 +39,9 @@ return {
                         return '<Ignore>'
                     end, { expr = true, desc = 'Go to next hunk' })
 
-                    map('n', '<leader>hp', function()
+                    map('n', '<leader>hN', function()
                         if vim.wo.diff then
-                            return '<leader>hp'
+                            return '<leader>hN'
                         end
                         vim.schedule(function()
                             gs.prev_hunk()
@@ -61,7 +61,7 @@ return {
                     map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
                     map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Stage Buffer' })
                     map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
-                    map('n', '<leader>hv', gs.preview_hunk, { desc = 'Preview Hunk' })
+                    map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk' })
                     map('n', '<leader>hb', function()
                         gs.blame_line({ full = true })
                     end, { desc = 'Blame line' })
