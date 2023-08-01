acaec2564e2cb47592489764a8db570054133300
Ilias <dougias.ilias@gmail.com>
2023-08-01 12:18:43 +0300

    Install nvim-alt-substitute

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -362,6 +362,12 @@ return {
             },
         },
     },
+    {
+        'chrisgrieser/nvim-alt-substitute',
+        opts = true,
+        -- lazy-loading with `cmd =` does not work well with incremental preview
+        event = 'CmdlineEnter',
+    },
 
     --  ╭──────────────────────────────────────────────────────────╮
     --  │                         WRITING                          │
