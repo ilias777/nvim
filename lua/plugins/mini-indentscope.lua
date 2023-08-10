7602787d2cf23609ec83b339890c9811d8dda02e
Ilias <dougias.ilias@gmail.com>
2023-08-10 04:02:04 +0200

    Disable mini.indentscope for specific filetypes

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -6,6 +6,18 @@ return {
     init = function()
         local colors = require('catppuccin.palettes').get_palette('mocha')
         vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = colors.sapphire })
+        vim.api.nvim_create_autocmd('Filetype', {
+            pattern = {
+                'help',
+                'Trouble',
+                'lazy',
+                'mason',
+                'notify',
+            },
+            callback = function()
+                vim.b.miniindentscope_disable = true
+            end,
+        })
     end,
     opts = {
         options = { try_as_border = true },
