ab1b87a2f53171605fbb479249a5c0ac8e5463e1
Ilias <dougias.ilias@gmail.com>
2023-08-10 02:08:38 +0200

    Change config function to opts table for focus

--- lua/plugins/focus.lua
+++ lua/plugins/focus.lua
@@ -9,64 +9,24 @@ return {
         { '<space>l', '<cmd>FocusSplitRight<CR>', desc = 'Focus Split Right' },
         { '<space>m', '<cmd>FocusMaxOrEqual<CR>', desc = 'Maximaze or Equalises Split' },
     },
-    config = function()
-        require('focus').setup({
-            autoresize = {
-                enable = false,
-            },
-            ui = {
-                number = false,
-                relativenumber = false,
-                hybridnumber = false,
-                signcolumn = false,
-            },
-            -- excluded_filetypes = {
-            --     'toggleterm',
-            --     'qf',
-            --     'help',
-            --     'NvimTree',
-            --     'Trouble',
-            --     'tsplayground',
-            --     'Outline',
-            -- },
-        })
-
-        -- Ignore filetypes
-        local ignore_filetypes = {
-            'toggleterm',
-            'qf',
-            'help',
-            'NvimTree',
-            'Trouble',
-            'tsplayground',
-            'Outline',
-        }
-        -- Ignore buftypes
-        local ignore_buftypes = {
-            'nofile',
-            'prompt',
-            'popup',
-        }
-
-        local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
-        vim.api.nvim_create_autocmd('WinEnter', {
-            group = augroup,
-            callback = function(_)
-                if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-                    vim.b.focus_disable = true
-                end
-            end,
-            desc = 'Disable focus autoresize for BufType',
-        })
-
-        vim.api.nvim_create_autocmd('FileType', {
-            group = augroup,
-            callback = function(_)
-                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-                    vim.b.focus_disable = true
-                end
-            end,
-            desc = 'Disable focus autoresize for FileType',
-        })
-    end,
+    opts = {
+        autoresize = {
+            enable = false,
+        },
+        ui = {
+            number = false,
+            relativenumber = false,
+            hybridnumber = false,
+            signcolumn = false,
+        },
+        -- excluded_filetypes = {
+        --     'toggleterm',
+        --     'qf',
+        --     'help',
+        --     'NvimTree',
+        --     'Trouble',
+        --     'tsplayground',
+        --     'Outline',
+        -- },
+    },
 }
