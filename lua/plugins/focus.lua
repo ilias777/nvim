b93c3bd0b17fd5760434e0614d1e071f22e9760c
Ilias <dougias.ilias@gmail.com>
2023-08-14 09:36:13 +0200

    Add autocommands for focus.nvim

--- lua/plugins/focus.lua
+++ lua/plugins/focus.lua
@@ -9,6 +9,32 @@ return {
         { '<space>l', '<cmd>FocusSplitRight<CR>', desc = 'Focus Split Right' },
         { '<space>m', '<cmd>FocusMaxOrEqual<CR>', desc = 'Maximaze or Equalises Split' },
     },
+    init = function()
+        local ignore_filetypes = { 'neo-tree', 'Trouble' }
+        local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+        local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+        vim.api.nvim_create_autocmd('WinEnter', {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+                    vim.b.focus_disable = true
+                end
+            end,
+            desc = 'Disable focus autoresize for BufType',
+        })
+
+        vim.api.nvim_create_autocmd('FileType', {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                    vim.b.focus_disable = true
+                end
+            end,
+            desc = 'Disable focus autoresize for FileType',
+        })
+    end,
     opts = {
         autoresize = {
             enable = false,
