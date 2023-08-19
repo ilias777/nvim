e02adc3d99ae7412abaa218b569348a30af51cf9
Ilias <dougias.ilias@gmail.com>
2023-08-19 15:26:49 +0200

    Change configuration for cmp mappings

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -71,17 +71,29 @@ cmp.setup({
         },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        -- ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        -- ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        -- ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        -- ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        -- ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<S-CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -105,11 +117,11 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
         { name = 'luasnip' },
         { name = 'buffer', max_view_entries = 10 },
-        { name = 'nvim_lua', max_view_entries = 10 },
         { name = 'path' },
-        { name = 'nvim_lsp_signature_help' },
+        { name = 'nvim_lua', max_view_entries = 10 },
         { name = 'treesitter', max_view_entries = 10 },
         -- { name = 'spell', option = {
         --     keep_all_entries = false,
@@ -129,11 +141,11 @@ cmp.setup({
             -- vim.item.kind = string.format('%s', kind_icons.kind)
             -- Source
             vim_item.menu = ({
+                nvim_lsp = '( LSP )',
+                nvim_lsp_signature_help = '( Signature )',
                 luasnip = '( LuaSnip )',
                 buffer = '( Buffer )',
-                nvim_lsp = '( LSP )',
                 path = '( Path )',
-                nvim_lsp_signature_help = '( Sign.Help )',
                 nvim_lua = '( Lua )',
                 treesitter = '( Treesitter )',
                 -- spell = '( Spell )',
