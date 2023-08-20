1d3e5aa502fab9c129f93c3448dc503d2704262b
Ilias <dougias.ilias@gmail.com>
2023-08-21 00:55:21 +0200

    Change keymaps for gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -29,48 +29,48 @@ return {
                     end
 
                     -- Navigation
-                    map('n', '<leader>hn', function()
+                    map('n', '<leader>gn', function()
                         if vim.wo.diff then
-                            return '<leader>hn'
+                            return '<leader>gn'
                         end
                         vim.schedule(function()
                             gs.next_hunk()
                         end)
                         return '<Ignore>'
-                    end, { expr = true, desc = 'Go to next hunk' })
+                    end, { expr = true, desc = 'Go to next git hunk' })
 
-                    map('n', '<leader>hN', function()
+                    map('n', '<leader>gN', function()
                         if vim.wo.diff then
-                            return '<leader>hN'
+                            return '<leader>gN'
                         end
                         vim.schedule(function()
                             gs.prev_hunk()
                         end)
                         return '<Ignore>'
-                    end, { expr = true, desc = 'Go to previous hunk' })
+                    end, { expr = true, desc = 'Go to previous git hunk' })
 
                     -- Actions
-                    map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Hunk' })
-                    map('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset Hunk' })
-                    map('v', '<leader>hs', function()
+                    map('n', '<leader>gs', gs.stage_hunk, { desc = 'Stage Hunk' })
+                    map('n', '<leader>gr', gs.reset_hunk, { desc = 'Reset Hunk' })
+                    map('v', '<leader>gs', function()
                         gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
                     end, { desc = 'Stage Hunk' })
-                    map('v', '<leader>hr', function()
+                    map('v', '<leader>gr', function()
                         gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
                     end, { desc = 'Reset Hunk' })
-                    map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
-                    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Stage Buffer' })
-                    map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
-                    map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk' })
-                    map('n', '<leader>hb', function()
+                    map('n', '<leader>gS', gs.stage_buffer, { desc = 'Stage Buffer' })
+                    map('n', '<leader>gu', gs.undo_stage_hunk, { desc = 'Undo Stage Buffer' })
+                    map('n', '<leader>gR', gs.reset_buffer, { desc = 'Reset Buffer' })
+                    map('n', '<leader>gp', gs.preview_hunk, { desc = 'Preview Hunk' })
+                    map('n', '<leader>gB', function()
                         gs.blame_line({ full = true })
-                    end, { desc = 'Blame line' })
-                    map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Git Toggle current blame line' })
-                    map('n', '<leader>hd', gs.diffthis, { desc = 'Hunk Diffthis' })
-                    map('n', '<leader>hD', function()
+                    end, { desc = 'Blame line full' })
+                    map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = 'Toggle current blame line' })
+                    map('n', '<leader>gd', gs.diffthis, { desc = 'Hunk Diffthis' })
+                    map('n', '<leader>gD', function()
                         gs.diffthis('~')
                     end, { desc = 'Hunk Diffthis' })
-                    map('n', '<leader>td', gs.toggle_deleted, { desc = 'Git Toggle deleted lines' })
+                    map('n', '<leader>gt', gs.toggle_deleted, { desc = 'Git Toggle deleted lines' })
 
                     -- Text object
                     map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
