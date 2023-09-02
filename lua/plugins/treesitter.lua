22f9975873bf6448a0c3311dad7a879edd2542bf
Ilias <dougias.ilias@gmail.com>
2023-09-03 00:00:05 +0200

    Set max_line to 4 for treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,12 @@ return {
             -- TS Treehopper
             { 'mfussenegger/nvim-treehopper' },
             -- TS Context
-            { 'nvim-treesitter/nvim-treesitter-context' },
+            {
+                'nvim-treesitter/nvim-treesitter-context',
+                opts = {
+                    max_lines = 4,
+                },
+            },
             -- TS Node-Action
             {
                 'ckolkey/ts-node-action',
