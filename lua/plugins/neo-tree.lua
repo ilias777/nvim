cad0b10920a85594e61e3147977a287774c842f4
Ilias <dougias.ilias@gmail.com>
2023-09-12 00:44:39 +0200

    Open and close folders with h and l

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -40,7 +40,25 @@ return {
     opts = {
         window = {
             mappings = {
-                ['o'] = 'open',
+                -- ['o'] = 'open',
+                ['h'] = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == 'directory' and node:is_expanded() then
+                        require('neo-tree.sources.filesystem').toggle_directory(state, node)
+                    else
+                        require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+                    end
+                end,
+                ['l'] = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == 'directory' then
+                        if not node:is_expanded() then
+                            require('neo-tree.sources.filesystem').toggle_directory(state, node)
+                        elseif node:has_children() then
+                            require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+                        end
+                    end
+                end,
                 ['e'] = function()
                     vim.api.nvim_exec('Neotree focus filesystem right', true)
                 end,
