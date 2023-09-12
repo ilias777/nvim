9ba23d3b9588887284780f03c39ec34481ee70e7
Ilias <dougias.ilias@gmail.com>
2023-09-12 11:50:32 +0200

    Open file with "l" if node is a file in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -40,7 +40,6 @@ return {
     opts = {
         window = {
             mappings = {
-                -- ['o'] = 'open',
                 ['h'] = function(state)
                     local node = state.tree:get_node()
                     if node.type == 'directory' and node:is_expanded() then
@@ -51,12 +50,16 @@ return {
                 end,
                 ['l'] = function(state)
                     local node = state.tree:get_node()
+                    local path = node:get_id()
                     if node.type == 'directory' then
                         if not node:is_expanded() then
                             require('neo-tree.sources.filesystem').toggle_directory(state, node)
                         elseif node:has_children() then
                             require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
                         end
+                    end
+                    if node.type == 'file' then
+                        require('neo-tree.utils').open_file(state, path)
                     end
                 end,
                 ['e'] = function()
