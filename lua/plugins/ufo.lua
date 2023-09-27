9cb0c6a3f393f294ac2632a69cde012789e3981b
Ilias <dougias.ilias@gmail.com>
2023-09-28 01:42:37 +0200

    Cleanup ufo.lua file

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -57,39 +57,4 @@ return {
             end,
         })
     end,
-    -- opts = function(_, opts)
-    --     vim.o.foldcolumn = '1'
-    --     vim.o.foldlevel = 99
-    --     vim.o.foldlevelstart = 99
-    --     vim.o.foldenable = true
-    --     -- To show number of folded lines
-    --     local handler = function(virtText, lnum, endLnum, width, truncate)
-    --         local newVirtText = {}
-    --         local suffix = ('  %d '):format(endLnum - lnum)
-    --         local sufWidth = vim.fn.strdisplaywidth(suffix)
-    --         local targetWidth = width - sufWidth
-    --         local curWidth = 0
-    --         for _, chunk in ipairs(virtText) do
-    --             local chunkText = chunk[1]
-    --             local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    --             if targetWidth > curWidth + chunkWidth then
-    --                 table.insert(newVirtText, chunk)
-    --             else
-    --                 chunkText = truncate(chunkText, targetWidth - curWidth)
-    --                 local hlGroup = chunk[2]
-    --                 table.insert(newVirtText, { chunkText, hlGroup })
-    --                 chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    --                 -- str width returned from truncate() may less than 2nd argument, need padding
-    --                 if curWidth + chunkWidth < targetWidth then
-    --                     suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-    --                 end
-    --                 break
-    --             end
-    --             curWidth = curWidth + chunkWidth
-    --         end
-    --         table.insert(newVirtText, { suffix, 'MoreMsg' })
-    --         return newVirtText
-    --     end
-    --     opts.fold_virt_text_handler = handler
-    -- end,
 }
