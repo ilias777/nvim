a5719ae3503d86c3008886882892a1434781c418
Ilias <dougias.ilias@gmail.com>
2023-09-28 14:08:19 +0200

    Update indent-blankline to version 3

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,35 +1,13 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
     event = 'BufReadPre',
-    config = function()
-        vim.cmd([[highlight IndentBlanklineIndent1 guifg=#f0c6c6 gui=nocombine]])
-        vim.cmd([[highlight IndentBlanklineIndent2 guifg=#c6a0f6 gui=nocombine]])
-        vim.cmd([[highlight IndentBlanklineIndent3 guifg=#ee99a0 gui=nocombine]])
-        vim.cmd([[highlight IndentBlanklineIndent4 guifg=#b7bdf8 gui=nocombine]])
-        vim.cmd([[highlight IndentBlanklineIndent5 guifg=#8bd5ca gui=nocombine]])
-        vim.cmd([[highlight IndentBlanklineIndent6 guifg=#b8c0e0 gui=nocombine]])
-
-        require('indent_blankline').setup({
-            space_char_blankline = ' ',
-            show_current_context = false,
-            show_current_context_start = false,
-            use_treesitter = true,
-            filetype_exclude = {
-                'NvimTree',
-                'dashboard',
-                'help',
-                'lspinfo',
-                'LspInstallerInfo',
-                'packer',
-            },
-            char_highlight_list = {
-                -- 'IndentBlanklineIndent1',
-                -- 'IndentBlanklineIndent2',
-                -- 'IndentBlanklineIndent3',
-                -- 'IndentBlanklineIndent4',
-                -- 'IndentBlanklineIndent5',
-                -- 'IndentBlanklineIndent6',
-            },
-        })
-    end,
+    main = 'ibl',
+    opts = {
+        indent = {
+            char = '│',
+        },
+        scope = {
+            enabled = false,
+        },
+    },
 }
