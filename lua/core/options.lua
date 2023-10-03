2ada032adebd500f89f9ebc7938c96483714df11
Ilias <dougias.ilias@gmail.com>
2023-10-03 09:14:47 +0200

    Comment some fold options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -45,9 +45,9 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.opt.foldmethod = 'expr'
-vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- vim.opt.foldmethod = 'expr'
+-- vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
+-- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
 -- Clipboard
 vim.opt.clipboard = 'unnamedplus'
