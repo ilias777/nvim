88f527516085f4598eaac085b9fff2a8719a7349
Ilias <dougias.ilias@gmail.com>
2023-10-07 20:55:54 +0200

    Update statuscol.nvim to show gitsigns

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -256,7 +256,7 @@ return {
             require('statuscol').setup({
                 relculright = true,
                 segments = {
-                    { sign = { name = { '.*' }, maxwidth = 1, colwidth = 1, auto = false }, click = 'v:lua.ScSa' },
+                    { sign = { namespace = { 'gitsigns*' }, maxwidth = 1, colwidth = 1, auto = false }, click = 'v:lua.ScSa' },
                     { sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
                     { text = { builtin.lnumfunc, '  ' }, click = 'v:lua.ScLa' },
                     { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
