d1f715845137a43175172aedf78b67a112989700
Ilias <dougias.ilias@gmail.com>
2023-10-15 18:07:23 +0300

    Change diagnostic highlights in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,6 +4,7 @@ return {
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
+        local mocha = require('catppuccin.palettes').get_palette('mocha')
         require('bufferline').setup({
             options = {
                 -- indicator = {
@@ -19,12 +20,8 @@ return {
                 tab_size = 20,
                 diagnostics = 'nvim_lsp',
                 diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                    local s = ' '
-                    for e, n in pairs(diagnostics_dict) do
-                        local sym = e == 'error' and ' ' or (e == 'warning' and '  ' or '󰌵 ')
-                        s = s .. sym .. n
-                    end
-                    return s
+                    local ret = (diagnostics_dict.error and ' ' .. diagnostics_dict.error .. ' ' or '') .. (diagnostics_dict.warning and ' ' .. diagnostics_dict.warning or '')
+                    return vim.trim(ret)
                 end,
                 ---@type 'thin' | 'thick' | 'slant' | 'padded_slant' | 'slope' | 'padded_slope'
                 separator_style = 'thin',
@@ -40,12 +37,51 @@ return {
             highlights = require('catppuccin.groups.integrations.bufferline').get({
                 custom = {
                     all = {
+                        -- Fill
                         fill = { bg = 'NONE' },
+                        -- Background
                         background = { bg = 'NONE' },
+                        -- Separator
                         separator = { bg = 'NONE' },
+                        -- Close button
                         close_button = { bg = 'NONE' },
+                        -- Duplicate
                         duplicate = { bg = 'NONE' },
+                        -- Modified
                         modified = { bg = 'NONE' },
+                        -- Diagnostics
+                        diagnostic = { bg = 'NONE' },
+                        diagnostic_visible = { bg = 'NONE' },
+                        diagnostic_selected = { bg = 'NONE' },
+                        -- Errors
+                        ---@diagnostic disable: need-check-nil
+                        error = { fg = mocha.surface1, bg = 'NONE' },
+                        error_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        error_selected = { bg = 'NONE' },
+                        error_diagnostic = { fg = mocha.surface1, bg = 'NONE' },
+                        error_diagnostic_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        error_diagnostic_selected = { bg = 'NONE' },
+                        -- Warnings
+                        warning = { fg = mocha.surface1, bg = 'NONE' },
+                        warning_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        warning_selected = { bg = 'NONE' },
+                        warning_diagnostic = { fg = mocha.surface1, bg = 'NONE' },
+                        warning_diagnostic_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        warning_diagnostic_selected = { bg = 'NONE' },
+                        -- Info
+                        info = { fg = mocha.surface1, bg = 'NONE' },
+                        info_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        info_selected = { bg = 'NONE' },
+                        info_diagnostic = { fg = mocha.surface1, bg = 'NONE' },
+                        info_diagnostic_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        info_diagnostic_selected = { bg = 'NONE' },
+                        -- Hint
+                        hint = { fg = mocha.surface1, bg = 'NONE' },
+                        hint_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        hint_selected = { bg = 'NONE' },
+                        hint_diagnostic = { fg = mocha.surface1, bg = 'NONE' },
+                        hint_diagnostic_visible = { fg = mocha.surface1, bg = 'NONE' },
+                        hint_diagnostic_selected = { bg = 'NONE' },
                     },
                 },
             }),
