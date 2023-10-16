78732fc7bfbe2508f16a9d9b784a8ad7d27ef2ad
Ilias <dougias.ilias@gmail.com>
2023-10-16 23:58:32 +0300

    Install dashboard.nvim

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,36 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    opts = {
+        theme = 'doom',
+        config = {
+            header = {
+                '',
+                '',
+                '',
+                ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+                ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+                ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+                ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+                ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+                ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+                '',
+            },
+            center = {
+                { action = 'Telescope find_files', desc = ' Find file', icon = ' ', key = 'f' },
+                { action = 'enew | startinsert', desc = ' New file', icon = ' ', key = 'n' },
+                { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
+                { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
+                { action = 'e lua/plugins/init.lua', desc = ' Config', icon = ' ', key = 'c' },
+                { action = 'Lazy', desc = ' Lazy', icon = '💤 ', key = 'l' },
+                { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
+            },
+            footer = function()
+                local stats = require('lazy').stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+            end,
+        },
+    },
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
