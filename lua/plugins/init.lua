e3486b9c08daf7aeb54012e6301da47a6924e502
Ilias <dougias.ilias@gmail.com>
2023-10-16 08:43:20 +0300

    Add cmd flag for aerial.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -118,6 +118,7 @@ return {
     {
         'stevearc/aerial.nvim',
         enabled = true,
+        cmd = 'AerialToggle',
         dependencies = {
             'nvim-treesitter/nvim-treesitter',
             'nvim-tree/nvim-web-devicons',
