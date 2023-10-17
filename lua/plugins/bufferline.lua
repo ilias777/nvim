e38613540752d7ebd9d9306d15bb2a0f9241701c
Ilias <dougias.ilias@gmail.com>
2023-10-17 15:18:50 +0300

    Fix diagnostics for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -19,7 +19,7 @@ return {
                 tab_size = 20,
                 diagnostics = 'nvim_lsp',
                 diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                    local ret = (diagnostics_dict.error and ' ' .. diagnostics_dict.error .. ' ' or '') .. (diagnostics_dict.warning and ' ' .. diagnostics_dict.warning or '') .. (diagnostics_dict.hint and '' .. diagnostics_dict.hint .. ' ' or '')
+                    local ret = (diagnostics_dict.error and ' ' .. diagnostics_dict.error .. ' ' or '') .. (diagnostics_dict.warning and ' ' .. diagnostics_dict.warning or '') .. (diagnostics_dict.hint and ' ' .. diagnostics_dict.hint .. ' ' or '')
                     return vim.trim(ret)
                 end,
                 ---@type 'thin' | 'thick' | 'slant' | 'padded_slant' | 'slope' | 'padded_slope'
