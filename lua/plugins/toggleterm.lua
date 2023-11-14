bcb7d771da031a4c9205e81639e4aa9544762ea2
Ilias <dougias.ilias@gmail.com>
2023-11-14 12:26:07 +0200

    Remove opts table from toggleterm configuration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,8 +5,4 @@ return {
         { '<space>t', '<cmd>ToggleTerm<cr>', desc = 'ToggleTerm' },
     },
     config = true,
-    opts = {
-        open_mapping = [[<c-7>]],
-        direction = 'float',
-    },
 }
