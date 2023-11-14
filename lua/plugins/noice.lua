2d2ebb7239e7aaf45b415cc22d66d4a12c0fd3ad
Ilias <dougias.ilias@gmail.com>
2023-11-14 21:26:44 +0200

    Change messages view_history in noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,7 +3,7 @@ return {
     event = 'VeryLazy',
     opts = {
         messages = {
-            view_history = 'notify',
+            view_history = 'messages',
         },
         commands = {
             history = {
