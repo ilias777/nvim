851dd49faa01c04f74f590264b3ec2490fd4e8d9
Ilias <dougias.ilias@gmail.com>
2023-11-14 21:34:29 +0200

    Add clipboard messages to mini view in noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -66,6 +66,7 @@ return {
                     event = 'notify',
                     any = {
                         { find = 'hidden' },
+                        { find = 'clipboard' },
                     },
                 },
             },
