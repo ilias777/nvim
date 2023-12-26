8a7302fe8a7367ea5b45c9b5613c6a551d5c7ce7
Ilias <dougias.ilias@gmail.com>
2023-12-26 10:29:54 +0100

    Add christmas strategy on rainbow-delimiters

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -4,10 +4,13 @@ return {
     event = 'BufReadPre',
     config = function()
         local rainbow_delimiters = require('rainbow-delimiters')
+        local christmas = require('rainbow-delimiters.strategy.christmas')
+        local christmas_lights = christmas.lights(rainbow_delimiters.strategy['global'])
 
         vim.g.rainbow_delimiters = {
             strategy = {
-                [''] = rainbow_delimiters.strategy['global'],
+                [''] = christmas_lights,
+                -- [''] = rainbow_delimiters.strategy['global'],
                 vim = rainbow_delimiters.strategy['local'],
             },
             query = {
