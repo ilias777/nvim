f7e3aaccaf01e03f66b483857af65fcc5f3bc6bb
Ilias <dougias.ilias@gmail.com>
2024-02-04 01:47:43 +0100

    Set guicursor to blink

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -59,6 +59,11 @@ vim.opt.backspace = { 'indent', 'eol', 'start' }
 -- Cursor
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
+vim.opt.guicursor = {
+    'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
+}
 
 -- Spell
 vim.opt.spell = false
