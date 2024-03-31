146035a8031bdf950b835486bd2863020e28967f
Ilias <dougias.ilias@gmail.com>
2024-03-31 12:31:44 +0300

    Switch from ultimate-autopair to nvim-autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,6 @@
 return {
     'windwp/nvim-autopairs',
-    enabled = false,
+    enabled = true,
     event = 'InsertEnter',
     config = function()
         require('nvim-autopairs').setup({
@@ -18,32 +18,38 @@ return {
             -- Rule for a pair with left-side ' ' and right side ' '
             Rule(' ', ' ')
                 -- Pair will only occur if the conditional function returns true
-                :with_pair(function(opts)
-                    -- We are checking if we are inserting a space in (), [], or {}
-                    local pair = opts.line:sub(opts.col - 1, opts.col)
-                    return vim.tbl_contains({
-                        brackets[1][1] .. brackets[1][2],
-                        brackets[2][1] .. brackets[2][2],
-                        brackets[3][1] .. brackets[3][2],
-                    }, pair)
-                end)
+                :with_pair(
+                    function(opts)
+                        -- We are checking if we are inserting a space in (), [], or {}
+                        local pair = opts.line:sub(opts.col - 1, opts.col)
+                        return vim.tbl_contains({
+                            brackets[1][1] .. brackets[1][2],
+                            brackets[2][1] .. brackets[2][2],
+                            brackets[3][1] .. brackets[3][2],
+                        }, pair)
+                    end
+                )
                 :with_move(cond.none())
                 :with_cr(cond.none())
                 -- We only want to delete the pair of spaces when the cursor is as such: ( | )
-                :with_del(function(opts)
-                    local col = vim.api.nvim_win_get_cursor(0)[2]
-                    local context = opts.line:sub(col - 1, col + 2)
-                    return vim.tbl_contains({
-                        brackets[1][1] .. '  ' .. brackets[1][2],
-                        brackets[2][1] .. '  ' .. brackets[2][2],
-                        brackets[3][1] .. '  ' .. brackets[3][2],
-                    }, context)
-                end),
+                :with_del(
+                    function(opts)
+                        local col = vim.api.nvim_win_get_cursor(0)[2]
+                        local context = opts.line:sub(col - 1, col + 2)
+                        return vim.tbl_contains({
+                            brackets[1][1] .. '  ' .. brackets[1][2],
+                            brackets[2][1] .. '  ' .. brackets[2][2],
+                            brackets[3][1] .. '  ' .. brackets[3][2],
+                        }, context)
+                    end
+                ),
         })
 
         -- ARROW KEY ON JAVASCRIPT
         npairs.add_rules({
-            Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' }):use_regex(true):set_end_pair_length(2),
+            Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+                :use_regex(true)
+                :set_end_pair_length(2),
         })
 
         -- AUTO ADDSPACE ON =
@@ -89,9 +95,11 @@ return {
                     :with_del(cond.none())
                     :use_key(bracket[2])
                     -- Removes the trailing whitespace that can occur without this
-                    :replace_map_cr(function(_)
-                        return '<C-c>2xi<CR><C-c>O'
-                    end),
+                    :replace_map_cr(
+                        function(_)
+                            return '<C-c>2xi<CR><C-c>O'
+                        end
+                    ),
             })
         end
 
--- lua/plugins/ultimate-autopair.lua
+++ lua/plugins/ultimate-autopair.lua
@@ -1,6 +1,6 @@
 return {
     'altermo/ultimate-autopair.nvim',
-    enabled = true,
+    enabled = false,
     event = { 'InsertEnter', 'CmdlineEnter' },
     branch = 'v0.6', --recomended as each new version will have breaking changes
     opts = {},
