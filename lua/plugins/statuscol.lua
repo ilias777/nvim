e8ba7e46a66d5049109b203206d6f171e46e8124
Ilias <dougias.ilias@gmail.com>
2024-04-29 14:29:35 +0200

    Fix diagnostic signs in statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -9,10 +9,13 @@ return {
                 { sign = { name = { 'Dap' }, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
                 { sign = { name = { 'todo*' }, maxwidth = 1 } },
                 {
+                    sign = { namespace = { 'diagnostic' }, maxwidth = 1, auto = false },
+                    click = 'v:lua.ScSa',
+                },
+                {
                     sign = { namespace = { 'gitsigns*' }, maxwidth = 1, colwidth = 1, auto = false },
                     click = 'v:lua.ScSa',
                 },
-                { sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
                 { text = { builtin.lnumfunc, '  ' }, click = 'v:lua.ScLa' },
                 { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
             },
