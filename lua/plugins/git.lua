a007358ea6e9f459d8cc352a405a346e9524f097
Ilias <dougias.ilias@gmail.com>
2024-04-29 17:29:01 +0200

    Add 'BufNewFile' event on gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,7 +15,7 @@ return {
     --  ╰──────────────────────────────────────────────────────────╯
     {
         'lewis6991/gitsigns.nvim',
-        event = 'BufReadPre',
+        event = { 'BufReadPre', 'BufNewFile' },
         dependencies = 'nvim-lua/plenary.nvim',
         config = function()
             require('gitsigns').setup({
