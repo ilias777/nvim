639b914cb6267d7ac91cdda734516f326d3601c2
Ilias <dougias.ilias@gmail.com>
2024-04-30 00:40:48 +0200

    Change Oil.nvim configuration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,13 @@
 return {
     'stevearc/oil.nvim',
     enabled = false,
-    lazy = false,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-        { '<Space>o', '<cmd>Oil<cr>', desc = 'File browser' },
+        { '<Space>o', '<cmd>Oil --float<cr>', desc = 'File browser' },
     },
-    opts = {},
-    config = true,
+    opts = {
+        float = {
+            max_width = 100,
+            max_height = 80,
+        },
+    },
 }
