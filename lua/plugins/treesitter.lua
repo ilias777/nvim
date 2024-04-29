27af061b471afc04fbf9a2439160acd521b5b533
Ilias <dougias.ilias@gmail.com>
2024-04-29 19:20:15 +0200

    Enable scope_incremental in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -67,7 +67,7 @@ return {
                 enable = true,
                 keymaps = {
                     init_selection = '<CR>',
-                    -- scope_incremental = '<CR>',
+                    scope_incremental = '<CR>',
                     node_incremental = '<TAB>',
                     node_decremental = '<S-TAB>',
                 },
