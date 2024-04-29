e5216e262e3d00ec503b618a1de9a26219e5862e
Ilias <dougias.ilias@gmail.com>
2024-04-29 16:35:38 +0200

    Move cmp-nvim-lsp from cmp to lspconfig

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,6 +21,7 @@ return {
         'neovim/nvim-lspconfig',
         event = { 'BufReadPre', 'BufReadPost', 'BufNewFile' },
         dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
             { 'folke/neodev.nvim', opts = {} },
         },
         config = function()
@@ -96,7 +97,6 @@ return {
         event = 'InsertEnter',
         dependencies = {
             'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'saadparwaiz1/cmp_luasnip',
             'ray-x/cmp-treesitter',
