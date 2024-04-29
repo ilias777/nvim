bb3aaff32ed4cc077b2773851342468ec99f2133
Ilias <dougias.ilias@gmail.com>
2024-04-29 14:08:44 +0200

    Add new event on todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
     'folke/todo-comments.nvim',
-    event = 'BufReadPre',
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = 'nvim-lua/plenary.nvim',
     config = function()
         require('todo-comments').setup({
