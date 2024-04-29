99193623004a8376e3d108b32a7660cd4220ab69
Ilias <dougias.ilias@gmail.com>
2024-04-29 16:12:25 +0200

    Cleanup nvim-surround.lua

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,11 +1,7 @@
 return {
     'kylechui/nvim-surround',
     enabled = true,
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'BufReadPre',
-    config = function()
-        require('nvim-surround').setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end,
+    version = '*',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = true,
 }
