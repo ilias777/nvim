3a6d9afeeaedb59246bebbf03fa5289e9e5f9ab8
Ilias <dougias.ilias@gmail.com>
2024-05-01 14:41:51 +0200

    Update nvim-autopairs configuration in cmp

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -260,6 +260,28 @@ cmp.setup.cmdline(':', {
 
 -- For nvim-autopairs
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local ts_utils = require('nvim-treesitter.ts_utils')
+
+local ts_node_func_parens_disabled = {
+    -- ecma
+    named_imports = true,
+    -- rust
+    use_declaration = true,
+}
+
+local default_handler = cmp_autopairs.filetypes['*']['('].handler
+cmp_autopairs.filetypes['*']['('].handler = function(char, item, bufnr, rules, commit_character)
+    local node_type = ts_utils.get_node_at_cursor():type()
+    if ts_node_func_parens_disabled[node_type] then
+        if item.data then
+            item.data.funcParensDisabled = true
+        else
+            char = ''
+        end
+    end
+    default_handler(char, item, bufnr, rules, commit_character)
+end
+
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
 -- local handlers = require('nvim-autopairs.completion.handlers')
