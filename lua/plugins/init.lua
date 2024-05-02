eadc04d42f1c8e4da9887e9bcdac522d5f15cc02
Ilias <dougias.ilias@gmail.com>
2024-05-03 01:40:06 +0200

    Chagne some icons for ouline.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,6 +84,26 @@ return {
         keys = {
             { '<space>o', '<cmd>Outline<cr>', desc = 'Toggle Outline' },
         },
+        opts = {
+            symbols = {
+                icons = {
+                    Module = { icon = '全' },
+                    Namespace = { icon = '' },
+                    Package = { icon = '' },
+                    Class = { icon = '' },
+                    Method = { icon = '' },
+                    Enum = { icon = '' },
+                    Interface = { icon = '' },
+                    Function = { icon = '' },
+                    Variable = { icon = '󰫧' },
+                    String = { icon = '󰅳' },
+                    Boolean = { icon = '' },
+                    Key = { icon = '' },
+                    Null = { icon = '󰟢' },
+                    Event = { icon = '' },
+                },
+            },
+        },
         config = true,
     },
     {
