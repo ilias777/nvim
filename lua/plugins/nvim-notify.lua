6e2e0375aae47c6ab77058fd3e64166145b0764b
Ilias <dougias.ilias@gmail.com>
2024-05-03 13:06:52 +0200

    Add notify configurations

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -12,5 +12,14 @@ return {
     opts = {
         render = 'compact',
         stages = 'slide',
+        max_height = function()
+            return math.floor(vim.o.lines * 0.75)
+        end,
+        max_width = function()
+            return math.floor(vim.o.columns * 0.75)
+        end,
+        on_open = function(win)
+            vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end,
     },
 }
