c5087fa1fd84090e0dcf4d6bb98d59872e519176
Ilias <dougias.ilias@gmail.com>
2024-05-03 13:24:19 +0200

    Remove custom icons from nvim-web-devicons

--- lua/plugins/web_devicons.lua
+++ lua/plugins/web_devicons.lua
@@ -9,18 +9,6 @@ return {
                 cterm_color = '86',
                 name = 'Toc',
             },
-            bib = {
-                icon = '󰌱',
-                color = '#d78700',
-                cterm_color = '172',
-                name = 'Bib',
-            },
-            -- vue = {
-            --     icon = '󰡄',
-            --     color = '#00d75f',
-            --     cterm_color = '41',
-            --     name = 'Vue',
-            -- },
         })
     end,
 }
