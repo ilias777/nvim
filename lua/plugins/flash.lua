ca028f240f45f9b22c148a955b0a8591874ff4f7
Ilias <dougias.ilias@gmail.com>
2024-05-16 15:34:08 +0200

    Enable backdrop for flash treesitter

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -24,6 +24,11 @@ return {
                     }
                 end,
             },
+            treesitter = {
+                highlight = {
+                    backdrop = true,
+                },
+            },
         },
     },
     keys = {
