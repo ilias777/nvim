5e4cd9fa6d994fec85fefe32c1b148be3ad26d0d
Ilias <dougias.ilias@gmail.com>
2024-05-17 23:13:32 +0200

    Change border for :lspinfo command to single

--- lua/plugins/lspconf/lsp-config.lua
+++ lua/plugins/lspconf/lsp-config.lua
@@ -45,7 +45,8 @@ local lsp_defaults = lspconfig.util.default_config
 lsp_defaults.capabilities =
     vim.tbl_deep_extend('force', lsp_defaults.capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-require('lspconfig.ui.windows').default_options.border = 'rounded'
+-- ────────────── LSP BORDER FOR :LSPINFO COMMAND ──────────────
+require('lspconfig.ui.windows').default_options.border = 'single'
 
 -- ──────────────── WINBAR WITH NAVIC ────────────────
 local navic = require('nvim-navic')
