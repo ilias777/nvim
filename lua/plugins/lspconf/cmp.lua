e6ea71a43d6755989e905b6f0442763101ee1799
Ilias <dougias.ilias@gmail.com>
2024-05-18 19:36:16 +0200

    Remove comment

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -62,7 +62,7 @@ local cmp = require('cmp')
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body)
         end,
     },
     window = {
