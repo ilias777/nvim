0e8cf662cd377a5f779142710f36a9b90080b6bc
Ilias <dougias.ilias@gmail.com>
2024-05-18 19:25:38 +0200

    Remove has_words_before function

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -54,14 +54,6 @@ local kind_icons = {
     -- Variable = ' ',
 }
 
--- ╭──────────────────────────╮
--- │ FOR LUASNIPS INTEGRATION │
--- ╰──────────────────────────╯
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
 -- ╭──────────────╮
 -- │ LOAD LUASNIP │
 -- ╰──────────────╯
