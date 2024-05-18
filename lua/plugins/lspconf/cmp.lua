ab963b68b600f929ab98b21b86690d341f1c4eaf
Ilias <dougias.ilias@gmail.com>
2024-05-18 19:34:45 +0200

    Fix path to load luasnip snippets

--- lua/plugins/lspconf/cmp.lua
+++ lua/plugins/lspconf/cmp.lua
@@ -5,7 +5,7 @@
 -- ╭───────────────╮
 -- │ LOAD SNIPPETS │
 -- ╰───────────────╯
-require('luasnip/loaders/from_lua').load({ paths = '~/.config/nvim/snippets/' })
+require('luasnip/loaders/from_lua').load({ paths = { '~/.config/nvim/snippets/' } })
 require('luasnip/loaders/from_vscode').lazy_load()
 
 -- ╭────────────────╮
