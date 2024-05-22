d54c3293b6efcc10fc964190db2a838018617b4b
Ilias <dougias.ilias@gmail.com>
2024-05-22 13:19:11 +0200

    Enable lsp process and ovverrides on noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,7 +12,12 @@ return {
         },
         lsp = {
             progress = {
-                enabled = false,
+                enabled = true,
+            },
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
             },
             hover = {
                 enabled = false,
