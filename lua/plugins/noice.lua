199b2b2c8254bafd1f55a3e21f782674aa9ffdd2
Ilias <dougias.ilias@gmail.com>
2024-05-22 13:31:24 +0200

    Disable again signature on noice.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,6 +16,9 @@ return {
                 ['vim.lsp.util.stylize_markdown'] = true,
                 ['cmp.entry.get_documentation'] = true,
             },
+            signature = {
+                enabled = false,
+            },
         },
         presets = {
             inc_rename = true,
