c05d0810598071410245f52e2a21b69ede982da1
Ilias <dougias.ilias@gmail.com>
2024-05-27 21:03:41 +0200

    Disable noice lsp progress

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,6 +11,9 @@ return {
             },
         },
         lsp = {
+            progress = {
+                enabled = false,
+            },
             override = {
                 ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
                 ['vim.lsp.util.stylize_markdown'] = true,
