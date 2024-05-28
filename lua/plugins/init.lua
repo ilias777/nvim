963f10e87829c91a4bfb31e5dea1dfd480ae8614
Ilias <dougias.ilias@gmail.com>
2024-05-28 02:09:53 +0200

    Remove luacheck from nvim-lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -192,7 +192,7 @@ return {
             lint.linters_by_ft = {
                 javascript = { 'eslint' },
                 json = { 'jsonlint' },
-                lua = { 'luacheck' },
+                -- lua = { 'luacheck' },
                 python = { 'ruff' },
                 vue = { 'eslint' },
                 yaml = { 'yamllint' },
