57c8059a407fa1ca79241ea95843c552f9732711
Ilias <dougias.ilias@gmail.com>
2024-05-31 00:56:49 +0200

    Change trouble keymaps to focus window when opened

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,17 +52,19 @@ return {
         keys = {
             { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
             { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
-            { '<leader>xs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Symbols (Trouble)' },
+            { '<leader>xs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
             {
                 '<leader>xl',
-                '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+                '<cmd>Trouble lsp toggle win.position=right<cr>',
                 desc = 'LSP Definitions / references / ... (Trouble)',
             },
             { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
             { '<leader>xq', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
             { '<leader>xt', '<cmd>TodoTrouble<cr>', desc = 'Todo Trouble' },
         },
-        config = true,
+        opts = {
+            focus = true,
+        },
     },
     {
         'dnlhc/glance.nvim',
