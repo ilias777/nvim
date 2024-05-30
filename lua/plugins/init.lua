6f5a9e832900bca1f823bc2993972c54b353a179
Ilias <dougias.ilias@gmail.com>
2024-05-31 01:16:57 +0200

    Change trouble.nvim keymaps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,17 +50,17 @@ return {
     {
         'folke/trouble.nvim',
         keys = {
-            { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
-            { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
-            { '<leader>xs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
+            { '<space>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
+            { '<space>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
+            { '<space>xs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
             {
-                '<leader>xr',
+                '<space>xr',
                 '<cmd>Trouble lsp toggle win.position=right<cr>',
                 desc = 'LSP Definitions / references / ... (Trouble)',
             },
-            { '<leader>xl', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
-            { '<leader>xq', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
-            { '<leader>xt', '<cmd>TodoTrouble<cr>', desc = 'Todo Trouble' },
+            { '<space>xl', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+            { '<space>xq', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
+            { '<space>xt', '<cmd>TodoTrouble<cr>', desc = 'Todo Trouble' },
         },
         opts = {
             focus = true,
