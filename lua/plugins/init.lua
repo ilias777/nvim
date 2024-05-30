1820c707e8cd059182c5b087b34d82251388cb4b
Ilias <dougias.ilias@gmail.com>
2024-05-31 01:14:29 +0200

    Change trouble.nvim keymaps again

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,11 +54,11 @@ return {
             { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
             { '<leader>xs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
             {
-                '<leader>xl',
+                '<leader>xr',
                 '<cmd>Trouble lsp toggle win.position=right<cr>',
                 desc = 'LSP Definitions / references / ... (Trouble)',
             },
-            { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+            { '<leader>xl', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
             { '<leader>xq', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
             { '<leader>xt', '<cmd>TodoTrouble<cr>', desc = 'Todo Trouble' },
         },
