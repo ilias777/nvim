f91d69537769bf4e6dd9307c92a79aa1713d9c8b
Ilias <dougias.ilias@gmail.com>
2024-06-02 10:07:32 +0200

    Add lazydev source to cmp

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -12,4 +12,14 @@ return {
         },
     },
     { 'Bilal2453/luvit-meta', lazy = true },
+    {
+        'hrsh7th/nvim-cmp',
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = 'lazydev',
+                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+            })
+        end,
+    },
 }
