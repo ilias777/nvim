4621088a2cb7e84cc2b0f3b4e8fa5e0576bf6b78
Ilias <dougias.ilias@gmail.com>
2024-06-02 09:57:23 +0200

    Update lazydev configuration

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -1,12 +1,15 @@
 return {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-        library = {
-            vim.env.LAZY .. '/luvit-meta/library', -- see below
-            -- You can also add plugins you always want to have loaded.
-            -- Useful if the plugin has globals or types you want to use
-            -- vim.env.LAZY .. "/LazyVim", -- see below
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                vim.env.LAZY .. '/luvit-meta/library', -- see below
+                -- You can also add plugins you always want to have loaded.
+                -- Useful if the plugin has globals or types you want to use
+                -- vim.env.LAZY .. "/LazyVim", -- see below
+            },
         },
     },
+    { 'Bilal2453/luvit-meta', lazy = true },
 }
