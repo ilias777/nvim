986a532c100a183c7f212bb1f3d45665596b865d
Ilias <dougias.ilias@gmail.com>
2024-06-04 19:18:02 +0200

    Add autocmd for document highlights on CursorHold

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -179,3 +179,17 @@ vim.api.nvim_create_autocmd('Filetype', {
         vim.bo.commentstring = '//%s'
     end,
 })
+
+-- ╭─────────────────────────────────────────────────────────╮
+-- │            DOCUMENT HIGHLIGHT ON CURSORHOLD             │
+-- ╰─────────────────────────────────────────────────────────╯
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    callback = function()
+        vim.lsp.buf.document_highlight()
+    end,
+})
+vim.api.nvim_create_autocmd('CursorMoved', {
+    callback = function()
+        vim.lsp.buf.clear_references()
+    end,
+})
