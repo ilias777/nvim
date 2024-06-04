4aa4093e3e239f616f731c82d85146a59b9714ef
Ilias <dougias.ilias@gmail.com>
2024-06-04 19:23:14 +0200

    Disable document highlights on CursorHold

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -183,13 +183,13 @@ vim.api.nvim_create_autocmd('Filetype', {
 -- ╭─────────────────────────────────────────────────────────╮
 -- │            DOCUMENT HIGHLIGHT ON CURSORHOLD             │
 -- ╰─────────────────────────────────────────────────────────╯
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    callback = function()
-        vim.lsp.buf.document_highlight()
-    end,
-})
-vim.api.nvim_create_autocmd('CursorMoved', {
-    callback = function()
-        vim.lsp.buf.clear_references()
-    end,
-})
+-- vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+--     callback = function()
+--         vim.lsp.buf.document_highlight()
+--     end,
+-- })
+-- vim.api.nvim_create_autocmd('CursorMoved', {
+--     callback = function()
+--         vim.lsp.buf.clear_references()
+--     end,
+-- })
