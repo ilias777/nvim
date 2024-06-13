27d09be7774b4884374b6e4ae0e9311340265e18
Ilias <dougias.ilias@gmail.com>
2024-06-13 12:30:57 +0200

    Change settings on fzf-lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -4,5 +4,21 @@ return {
     keys = {
         { '<M-p>', "<cmd>lua require('fzf-lua').files()<cr>", desc = 'FZF Files' },
     },
+    opts = {
+        winopts = {
+            border = 'single',
+        },
+        preview = {
+            hidden = 'hidden',
+        },
+        keymap = {
+            builtin = {
+                ['<C-d>'] = 'preview-page-down',
+                ['<C-u>'] = 'preview-page-up',
+                ['<S-down>'] = 'preview-page-down',
+                ['<S-up>'] = 'preview-page-up',
+            },
+        },
+    },
     config = true,
 }
