b57c9fccfd0cda8d906d543af1219d2d968c538c
Ilias <dougias.ilias@gmail.com>
2024-06-20 11:26:29 +0200

    Move conform.nvim to own file

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+    'stevearc/conform.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+        formatters_by_ft = {
+            css = { 'prettier' },
+            html = { 'prettier' },
+            java = { 'prettier' },
+            javascript = { 'prettier' },
+            json = { 'prettier' },
+            tex = { 'latexindent' },
+            lua = { 'stylua' },
+            markdown = { 'prettier' },
+            php = { 'prettier' },
+            python = { 'black' },
+            rust = { 'rustfmt' },
+            scss = { 'prettier' },
+            vue = { 'prettier' },
+            yaml = { 'yamlfmt' },
+        },
+        format_on_save = {
+            lsp_format = 'fallback',
+            async = false,
+            timeout_ms = 500,
+        },
+        -- formatters = {
+        --     latexindent = {
+        --         prepend_args = { '-l', '/Users/ilias/.indentconfig.yaml' },
+        --     },
+        -- },
+    },
+}
