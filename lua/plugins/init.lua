189caa10465a69dc3305db4576a1fa9cf0f83d3c
Ilias <dougias.ilias@gmail.com>
2024-06-21 01:42:31 +0200

    Add nvim-dap as dependencie for java-debug

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -214,6 +214,9 @@ return {
     {
         'mfussenegger/nvim-jdtls',
         ft = 'java',
+        dependencies = {
+            'mfussenegger/nvim-dap',
+        },
     },
 
     --  ╭──────────────────────────────────────────────────────────╮
