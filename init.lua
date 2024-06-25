f57cd4a8b3f3359dcc3c86972876d748140a8e18
Ilias <dougias.ilias@gmail.com>
2024-06-25 19:09:18 +0200

    Update lazy.nvim configuration

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
     vim.fn.system({
         'git',
         'clone',
         '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
         '--branch=stable', -- latest stable release
+        lazyrepo,
         lazypath,
     })
 end
