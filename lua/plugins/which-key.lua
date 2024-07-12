cda47787dee169bd307ce1f04261ccbbef706419
Ilias <dougias.ilias@gmail.com>
2024-07-13 00:18:01 +0200

    Upsate which-key to new configuration

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,46 +1,39 @@
 return {
     'folke/which-key.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = 'VeryLazy',
     enabled = true,
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 500
-    end,
     opts = {
-        window = {
-            border = 'single', -- none, single, double, shadow
-            position = 'bottom', -- bottom, top
-            margin = { 0, 10, 1, 10 }, -- extra window margin [top, right, bottom, left]
+        preset = 'helix',
+        win = {
+            border = 'single',
         },
-        layout = {
-            height = { min = 4, max = 15 }, -- min and max height of the columns
-            width = { min = 20, max = 50 }, -- min and max width of the columns
-            spacing = 2, -- spacing between columns
-            align = 'center', -- align columns left, center or right
-        },
-        defaults = {
-            ['gr'] = { name = '+LSP Functions' },
-            ['grw'] = { name = '+LSP Workspace Folders' },
-            ['<leader>c'] = { name = '+Comment-Box / Context Treesitter' }, -- Comment Box
-            ['<leader>d'] = { name = '+Diffview / Debug' }, -- Diffview & nvim-dap
-            ['<leader>ds'] = { name = '+Dap Step' }, -- nvim-dap step
-            ['<leader>f'] = { name = '+File' }, -- Telescope
-            ['<leader>g'] = { name = '+Git' }, -- Git
-            ['<leader>h'] = { name = '+Hop Motion' }, -- Hop
-            ['<leader>l'] = { name = '+Lazy / LTeX' }, -- Lazy & LTeX
-            ['<leader>m'] = { name = '+Messages' }, -- Messages
-            ['<leader>t'] = { name = '+ToggleTerm / Todo Comments' }, -- ToggleTerm & Todo Comments
-            ['<space>b'] = { name = '+Buffer' }, -- Bufferline.nvim
-            ['<space>g'] = { name = '+Glance LSP' }, -- Glance LSP locations
-            ['<space>i'] = { name = '+IncRename / Inlay Hints' }, -- IncRename
-            ['<space>n'] = { name = '+Noice' }, -- Noice.nvim
-            ['<Space>r'] = { name = '+Lsp Rename' }, -- Rename with LSP
-            ['<space>x'] = { name = '+Trouble' }, -- Trouble.nvim
+        sort = { 'manual', 'group', 'lower' },
+        spec = {
+            {
+                mode = { 'n', 'v' },
+                { 'gr', group = '+LSP Functions' },
+                { 'grw', group = '+LSP Workspace Folders' },
+                { '<leader>c', group = '+Comment-Box / Context Treesitter' }, -- Comment Box
+                { '<leader>d', group = '+Diffview / Debug' }, -- Diffview & nvim-dap
+                { '<leader>ds', group = '+Dap Step' }, -- nvim-dap step
+                { '<leader>f', group = '+File' }, -- Telescope
+                { '<leader>g', group = '+Git' }, -- Git
+                { '<leader>h', group = '+Hop Motion' }, -- Hop
+                { '<leader>l', group = '+Lazy / LTeX' }, -- Lazy & LTeX
+                { '<leader>m', group = '+Show Messages' }, -- Messages
+                { '<leader>t', group = '+ToggleTerm / Todo Comments' }, -- ToggleTerm & Todo Comments
+                { '<space>b', group = '+Buffer' }, -- Bufferline.nvim
+                { '<space>g', group = '+Glance LSP' }, -- Glance LSP locations
+                { '<space>i', group = '+IncRename / Inlay Hints' }, -- IncRename
+                { '<space>n', group = '+Noice' }, -- Noice.nvim
+                { '<Space>r', group = '+Lsp Rename' }, -- Rename with LSP
+                { '<space>x', group = '+Trouble' }, -- Trouble.nvim
+            },
         },
     },
     config = function(_, opts)
         local wk = require('which-key')
         wk.setup(opts)
-        wk.register(opts.defaults)
+        wk.add(opts.spec)
     end,
 }
