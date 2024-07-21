325be6e1760b95c0ef859bfeddead295c49ad686
Ilias <dougias.ilias@gmail.com>
2024-07-21 15:19:01 +0200

    fix(vim-visual-multi): Change configuration

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,12 +1,8 @@
 return {
     'mg979/vim-visual-multi',
     branch = 'master',
+    event = 'VeryLazy',
     keys = {
-        { '<C-n>' },
-        { '<C-Up>' },
-        { '<C-Down>' },
-        { '<C-Left>' },
-        { '<C-Right>' },
         { 'cp', 'vip<Plug>(VM-Visual-Cursors)', desc = 'Create multicursors inner paragraph' },
         { '<M-s>', ':VMSearch', mode = 'x', desc = 'Search & create multicursors in visual mode' },
         { '<M-s>', ':%VMSearch', desc = 'Search & create multicursors' },
@@ -14,8 +10,11 @@ return {
     },
     init = function()
         vim.g.VM_maps = {
+            ['Motion ,'] = '',
             ['Select l'] = '<C-Right>',
             ['Select h'] = '<C-Left>',
+            ['Goto Next'] = '',
+            ['Goto Prev'] = '',
             ['I BS'] = '',
         }
         -- Themes: 'codedark' | 'iceblue' | 'purplegray' | 'nord' | 'sand' | 'ocean' | 'olive' | 'spacesray'
