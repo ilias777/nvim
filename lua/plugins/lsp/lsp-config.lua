2a95606822fb99878b6cf32dc03f0160c8ba170c
Ilias <dougias.ilias@gmail.com>
2024-10-24 09:27:22 +0200

    feat(tailwindcss): Add settings to server

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -362,6 +362,27 @@ return {
         -- ╰─────────────────╯
         lspconfig.tailwindcss.setup({
             handlers = handlers,
+            settings = {
+                tailwindCSS = {
+                    classAttributes = { 'class', 'className', 'class:list', 'classList', 'ngClass' },
+                    includeLanguages = {
+                        eelixir = 'html-eex',
+                        eruby = 'erb',
+                        htmlangular = 'html',
+                        templ = 'html',
+                    },
+                    lint = {
+                        cssConflict = 'warning',
+                        invalidApply = 'error',
+                        invalidConfigPath = 'error',
+                        invalidScreen = 'error',
+                        invalidTailwindDirective = 'error',
+                        invalidVariant = 'error',
+                        recommendedVariantOrder = 'warning',
+                    },
+                    validate = true,
+                },
+            },
         })
 
         -- ╭─────────────╮
