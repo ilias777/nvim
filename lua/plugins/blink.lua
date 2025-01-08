b2bfe737fcca43d0cb5be95405305adc6690be37
Ilias <dougias.ilias@gmail.com>
2025-01-08 09:19:36 +0100

    fix(blink.cmp): Update list

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -29,9 +29,14 @@ return {
                 enabled = true,
             },
             list = {
-                selection = function(ctx)
-                    return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect'
-                end,
+                selection = {
+                    preselect = function(ctx)
+                        return ctx.mode ~= 'cmdline' and not require('blink.cmp').snippet_active({ direction = 1 })
+                    end,
+                    auto_insert = function(ctx)
+                        return ctx.mode ~= 'cmdline'
+                    end,
+                },
             },
         },
         signature = {
