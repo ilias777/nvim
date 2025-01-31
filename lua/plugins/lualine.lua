e314cfe6e366372879510abe41a537b0a94db379
Ilias <dougias.ilias@gmail.com>
2025-01-31 21:57:50 +0100

    feat(lualine): Add mason updates to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -170,6 +170,42 @@ return {
             separator = { left = icon.ui.PowerlineLeftRounded, right = icon.ui.PowerlineRightRounded },
         }
 
+        local function lualine_mason_updates()
+            local registry = require('mason-registry')
+            local installed_packages = registry.get_installed_package_names()
+            local upgrades_available = false
+            local packages_outdated = 0
+            function myCallback(success, result_or_err)
+                if success then
+                    upgrades_available = true
+                    packages_outdated = packages_outdated + 1
+                end
+            end
+
+            for _, pkg in pairs(installed_packages) do
+                local p = registry.get_package(pkg)
+                if p then
+                    p:check_new_version(myCallback)
+                end
+            end
+
+            if upgrades_available then
+                return packages_outdated
+            else
+                return ''
+            end
+        end
+
+        local mason_updates = {
+            lualine_mason_updates,
+            icon = ' ',
+            on_click = function()
+                vim.cmd('Mason')
+            end,
+            color = { fg = colors.green, bg = colors.crust },
+            separator = { left = icon.ui.PowerlineLeftRounded, right = icon.ui.PowerlineRightRounded },
+        }
+
         local dia = {
             'diagnostics',
             sources = { 'nvim_diagnostic' },
@@ -232,6 +268,8 @@ return {
                     dia,
                     space,
                     lazy_updates,
+                    space,
+                    mason_updates,
                     space,
                     lsp,
                 },
