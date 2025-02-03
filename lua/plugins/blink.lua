1da9457c6757ffa0c32707f912f21c448fcbf13a
Ilias <dougias.ilias@gmail.com>
2025-02-03 21:27:33 +0100

    feat(blink): lsp min_keyword_length to 0

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -99,8 +99,8 @@ return {
             end,
             providers = {
                 lsp = {
-                    min_keyword_length = 2, -- Number of characters to trigger porvider
-                    score_offset = 0, -- Boost/penalize the score of the items
+                    min_keyword_length = 0,
+                    score_offset = 0,
                 },
                 path = {
                     min_keyword_length = 0,
