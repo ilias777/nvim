301dcea5d99d2a57daf44c46aa1bcc1b4eba919d
Ilias <dougias.ilias@gmail.com>
2025-02-08 19:09:44 +0100

    Revert "refactor(snipe): Set position to center"
    This reverts commit f7ca0c8880ee459b1d0d4e61fe6bdfda2aff7918.

--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -12,7 +12,7 @@ return {
     opts = {
         ui = {
             --- @type "topleft" | "bottomleft" | "topright" | "bottomright" | "center" | "cursor"
-            position = 'center',
+            position = 'cursor',
             preselect_current = true,
             text_align = 'file-first',
         },
