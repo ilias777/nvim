aef95f02d46c2f8894ae0cf2f59b227dc838ccdc
Ilias <dougias.ilias@gmail.com>
2025-03-08 01:30:22 +0100

    refactor(multicursor): Update configuration

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -2,67 +2,86 @@ return {
     'jake-stewart/multicursor.nvim',
     branch = '1.0',
     config = function()
+        -- stylua: ignore start
+        local clr = require('catppuccin.palettes').get_palette()
         local mc = require('multicursor-nvim')
 
         mc.setup()
 
         local set = vim.keymap.set
 
-        -- Add or skip cursor above/below the main cursor.
-        set({ 'n', 'v' }, '<c-up>', function()
-            mc.lineAddCursor(-1)
-        end)
-        set({ 'n', 'v' }, '<c-down>', function()
-            mc.lineAddCursor(1)
-        end)
-        set({ 'n', 'v' }, '<leader><up>', function()
-            mc.lineSkipCursor(-1)
-        end)
-        set({ 'n', 'v' }, '<leader><down>', function()
-            mc.lineSkipCursor(1)
-        end)
+        -- ADD MATCH CURSOR
+        set({ 'n', 'v' }, '<c-n>', function() mc.matchAddCursor(1) end, { desc = 'Add multicursor match next' })
 
-        -- Add or skip adding a new cursor by matching word/selection
-        set({ 'n', 'v' }, '<c-n>', function()
-            mc.matchAddCursor(1)
-        end)
-        set({ 'n', 'v' }, '<leader>q', function()
-            mc.matchSkipCursor(1)
-        end)
-        set({ 'n', 'v' }, '<c-p>', function()
-            mc.matchAddCursor(-1)
-        end)
-        set({ 'n', 'v' }, '<leader>Q', function()
-            mc.matchSkipCursor(-1)
-        end)
+        -- ADD CURSOR UP/DOWN
+        set({ 'n', 'x' }, '<c-up>', function() mc.lineAddCursor(-1) end, { desc = 'Add multicursor up' })
+        set({ 'n', 'x' }, '<c-down>', function() mc.lineAddCursor(1) end, { desc = 'Add multicursor down' })
 
-        -- Add all matches in the document
-        set({ 'n', 'v' }, '<leader>A', mc.matchAllAddCursors)
+        -- TOGGLE CURSOR
+        set({ 'n', 'x' }, '<c-q>', mc.toggleCursor, { desc = 'Toggle multicursor' })
 
-        -- You can also add cursors with any motion you prefer:
-        set('n', '<c-right>', function()
-            mc.addCursor('w')
-        end)
-        set('n', '<c-right>', function()
-            mc.skipCursor('w')
-        end)
+        -- OPERATOR
+        set('n', 'ga', mc.addCursorOperator, { desc = 'Add multicursor operator' })
 
-        -- Rotate the main cursor.
-        set({ 'n', 'v' }, '<right>', mc.nextCursor)
-        set({ 'n', 'v' }, '<left>', mc.prevCursor)
+        -- CLONE EVERY CURSOR AND DISABLE THE ORIGINALS.
+        set({ 'n', 'x' }, '\\d', mc.duplicateCursors, { desc = 'Duplicate multicursor' })
 
-        -- Delete the main cursor.
-        set({ 'n', 'v' }, '<leader>x', mc.deleteCursor)
+        -- ALIGN CURSOR COLUMNS.
+        set('n', '\\a', mc.alignCursors, { desc = 'Align multicursor' })
 
-        -- Add and remove cursors with control + left click.
+        -- MATCH NEW CURSORS WITHIN VISUAL SELECTIONS BY REGEX.
+        set('x', '\\r', mc.matchCursors, { desc = 'Match multicursor in visual by regex' })
+
+        -- BRING BACK CURSORS IF YOU ACCIDENTALLY CLEAR THEM
+        set('n', '\\gv', mc.restoreCursors, { desc = 'Restore multicursor' })
+
+        -- ADD A CURSOR FOR ALL MATCHES OF CURSOR WORD/SELECTION IN THE DOCUMENT.
+        set({ 'n', 'x' }, '\\A', mc.matchAllAddCursors, { desc = 'Add multicursor under the cursor' })
+
+        -- APPEND/INSERT FOR EACH LINE OF VISUAL SELECTIONS.
+        -- SIMILAR TO BLOCK SELECTION INSERTION.
+        set('x', 'I', mc.insertVisual, { desc = 'Insert multicursor for each line' })
+        set('x', 'A', mc.appendVisual, { desc = 'Append multicursor for each line' })
+
+        -- INCREMENT/DECREMENT SEQUENCES, TREAING ALL CURSORS AS ONE SEQUENCE.
+        set({ 'n', 'x' }, 'g<c-a>', mc.sequenceIncrement, { desc = 'Increment sequence multicursor' })
+        set({ 'n', 'x' }, 'g<c-x>', mc.sequenceDecrement, { desc = 'Decrement sequence multicursor' })
+
+        -- ADD A CURSOR TO EVERY SEARCH RESULT IN THE BUFFER.
+        set('n', '\\/A', mc.searchAllAddCursors, { desc = 'Add multicursor to every search result' })
+
+        -- ADD A CURSOR AND JUMP TO THE NEXT/PREVIOUS SEARCH RESULT.
+        set('n', '\\\\n', function() mc.searchAddCursor(1) end, { desc = 'Add multicursor and jump to next search' })
+        set('n', '\\\\N', function() mc.searchAddCursor(-1) end, { desc = 'Add multicursor and jump to previous search' })
+
+        -- JUMP TO THE NEXT/PREVIOUS SEARCH RESULT WITHOUT ADDING A CURSOR.
+        set('n', '\\\\s', function() mc.searchSkipCursor(1) end, { desc = 'Jump to next search without multicursor' })
+        set('n', '\\\\S', function() mc.searchSkipCursor(-1) end, { desc = 'Jump to previous search without multicursor' })
+
+        -- PRESSING `<LEADER>MIWAP` WILL CREATE A CURSOR IN EVERY MATCH OF THE
+        -- STRING CAPTURED BY `IW` INSIDE RANGE `AP`.
+        -- THIS ACTION IS HIGHLY CUSTOMIZABLE, SEE `:H MULTICURSOR-OPERATOR`.
+        set({ 'n', 'x' }, '<leader>m', mc.operator, { desc = 'Multicursor operator' })
+
+        -- ADD/SKIP CURSOR BY WORD FORWARD OR BACKWARD
+        set('n', '<c-right>', function() mc.addCursor('w') end, { desc = 'Add multicursor word forward' })
+        set('n', '<c-left>', function() mc.addCursor('b') end, { desc = 'Add multicursor word backward' })
+        set('n', '\\<right>', function() mc.skipCursor('w') end, { desc = 'Skip multicursor word forward' })
+        set('n', '\\<left>', function() mc.skipCursor('b') end, { desc = 'Skip multicursor word backward' })
+
+        -- SPLIT VISUAL SELECTIONS BY REGEX.
+        set('x', '\\l', mc.splitCursors, { desc = 'Split multicursor by regex' })
+
+        -- ROTATE THE TEXT CONTAINED IN EACH VISUAL SELECTION BETWEEN CURSORS.
+        set("x", "\\t", function() mc.transposeCursors(1) end, { desc = 'Transponse multicursor down' })
+        set("x", "\\T", function() mc.transposeCursors(-1) end, { desc = 'Transponse multicursor up' })
+
+        -- MOUSE
         set('n', '<c-leftmouse>', mc.handleMouse)
+        set('n', '<c-leftdrag>', mc.handleMouseDrag)
+        set('n', '<c-leftrelease>', mc.handleMouseRelease)
 
-        -- Easy way to add and remove cursors using the main cursor.
-        set({ 'n', 'v' }, '<c-q>', mc.toggleCursor)
-
-        -- Clone every cursor and disable the originals.
-        set({ 'n', 'v' }, '<leader><c-q>', mc.duplicateCursors)
-
+        -- EXIT MULTICURSOR
         set('n', '<esc>', function()
             if not mc.cursorsEnabled() then
                 mc.enableCursors()
@@ -73,41 +92,32 @@ return {
             end
         end)
 
-        -- bring back cursors if you accidentally clear them
-        set('n', '<leader>gv', mc.restoreCursors)
+        mc.addKeymapLayer(function(layerSet)
+            layerSet({ 'n', 'x' }, '<left>', mc.prevCursor)
+            layerSet({ 'n', 'x' }, '<right>', mc.nextCursor)
+            layerSet({ 'n', 'x' }, '\\x', mc.deleteCursor)
+            layerSet({ 'n', 'x' }, 'q', function() mc.matchSkipCursor(1) end)
+            layerSet({ 'n', 'x' }, 'Q', function() mc.matchSkipCursor(-1) end)
+            layerSet({ 'n', 'x' }, 'n', function() mc.matchAddCursor(1) end)
+            layerSet({ 'n', 'x' }, 'N', function() mc.matchAddCursor(-1) end)
 
-        -- Align cursor columns.
-        set('n', '<leader>a', mc.alignCursors)
-
-        -- Split visual selections by regex.
-        set('v', 'L', mc.splitCursors)
-
-        -- Append/insert for each line of visual selections.
-        set('v', 'I', mc.insertVisual)
-        set('v', 'A', mc.appendVisual)
-
-        -- match new cursors within visual selections by regex.
-        set('v', 'M', mc.matchCursors)
-
-        -- Rotate visual selection contents.
-        set('v', '<leader>t', function()
-            mc.transposeCursors(1)
-        end)
-        set('v', '<leader>T', function()
-            mc.transposeCursors(-1)
+            layerSet("n", "<esc>", function()
+                if not mc.cursorsEnabled() then
+                    mc.enableCursors()
+                else
+                    mc.clearCursors()
+                end
+            end)
         end)
 
-        -- Jumplist support
-        set({ 'v', 'n' }, '<c-i>', mc.jumpForward)
-        set({ 'v', 'n' }, '<c-o>', mc.jumpBackward)
-
-        -- Customize how cursors look.
+        -- CUSTOMIZE HOW CURSORS LOOK.
         local hl = vim.api.nvim_set_hl
-        hl(0, 'MultiCursorCursor', { link = 'Cursor' })
+        hl(0, 'MultiCursorCursor', { bg = clr.red, fg = clr.mantle })
         hl(0, 'MultiCursorVisual', { link = 'Visual' })
         hl(0, 'MultiCursorSign', { link = 'SignColumn' })
         hl(0, 'MultiCursorDisabledCursor', { link = 'Visual' })
         hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
         hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+        -- stylua: ignore end
     end,
 }
