1917d3fd8a766395fff731955c15b74fbd1199de
Ilias <dougias.ilias@gmail.com>
2025-03-13 23:13:58 +0100

    feat(tinymist): Add OpenPdf user command

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -164,6 +164,12 @@ return {
                             { bufnr = ev.buf }
                         )
                     end, {})
+                    vim.api.nvim_create_user_command('OpenPdf', function()
+                        local filepath = vim.api.nvim_buf_get_name(0)
+                        if filepath:match('%.typ$') then
+                            os.execute('open ' .. vim.fn.shellescape(filepath:gsub('%.typ$', '.pdf')))
+                        end
+                    end, {})
                 end
             end,
         })
