f462cb8aadeae93be4c15ad18eff302deb1f31ed
Ilias <dougias.ilias@gmail.com>
2025-03-13 08:15:36 +0100

    feat(nvim-surround): Update to v.3.0.0

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,7 +1,7 @@
 return {
     'kylechui/nvim-surround',
     enabled = true,
-    version = '*',
+    version = '3.0.0',
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
         move_cursor = 'sticky',
