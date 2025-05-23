b6731546caf46f7b0dae8f3cef0fa35023cc8443
Ilias <dougias.ilias@gmail.com>
2025-05-23 23:14:18 +0200

    refactor(vue_ls): Add configuration

--- lua/config/lsp/servers/vue_ls.lua
+++ lua/config/lsp/servers/vue_ls.lua
@@ -1,1 +1,10 @@
+vim.lsp.config('vue_ls', {
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    init_options = {
+        vue = {
+            hybridMode = false,
+        },
+    },
+})
+
 vim.lsp.enable('vue_ls')
