32f9085c3ebcfff3b72d5d3fd7c9b873431963f7
Ilias <dougias.ilias@gmail.com>
2025-06-17 19:52:15 +0200

    refactor(nvim-lspconfig): Move global lsp configuration to nvim-lspconfig

--- init.lua
+++ init.lua
@@ -5,14 +5,3 @@ require('config.autocommands')
 require('config.usercommands')
 require('utils.init')
 require('config.cursor')
-
-require('config.lsp.global')
-
-local lsp_path = vim.fn.stdpath('config') .. '/lua/config/lsp/servers'
-
-for _, file in ipairs(vim.fn.readdir(lsp_path)) do
-    if file:match('%.lua$') and file ~= 'global.lua' then
-        local module_name = 'config.lsp.servers.' .. file:gsub('%.lua$', '')
-        require(module_name)
-    end
-end
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,5 +9,16 @@ return {
     config = function()
         require('mason').setup()
         require('mason-lspconfig').setup()
+
+        require('config.lsp.global')
+
+        local lsp_path = vim.fn.stdpath('config') .. '/lua/config/lsp/servers'
+
+        for _, file in ipairs(vim.fn.readdir(lsp_path)) do
+            if file:match('%.lua$') and file ~= 'global.lua' then
+                local module_name = 'config.lsp.servers.' .. file:gsub('%.lua$', '')
+                require(module_name)
+            end
+        end
     end,
 }
