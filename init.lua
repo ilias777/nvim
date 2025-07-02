2bca3f51dace156a314bb3cadad1c2d51e6a0f80
Ilias <dougias.ilias@gmail.com>
2025-07-02 07:42:50 +0200

    fix: Change load order

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-require('config.lazy')
 require('config.options')
 require('config.keymaps')
 require('config.autocommands')
 require('config.usercommands')
+require('config.lazy')
 require('utils.init')
 require('config.cursor')
