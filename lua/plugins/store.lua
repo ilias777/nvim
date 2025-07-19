d80e81d6f64f9e68b59fe0e797fb7802dbb3febe
Ilias <dougias.ilias@gmail.com>
2025-07-19 15:28:07 +0200

    feat(plugin): Install store.nvim

---
+++ lua/plugins/store.lua
@@ -0,0 +1,11 @@
+return {
+    'alex-popov-tech/store.nvim',
+    enabled = true,
+    dependencies = { 'OXY2DEV/markview.nvim' },
+    cmd = 'Store',
+    opts = {
+        modal = {
+            border = 'single',
+        },
+    },
+}
