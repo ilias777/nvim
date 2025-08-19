113ff27c73bfac00330857213adaf4baf8ed4564
Ilias <dougias.ilias@gmail.com>
2025-08-19 22:41:48 +0200

    fix(bufferline): Set get_theme() instead of get()

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -29,7 +29,7 @@ return {
                     },
                 },
             },
-            highlights = require('catppuccin.groups.integrations.bufferline').get(),
+            highlights = require('catppuccin.groups.integrations.bufferline').get_theme(),
         })
     end,
 }
