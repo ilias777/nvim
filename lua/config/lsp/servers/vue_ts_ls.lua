37276a5c8923b17800c47e99ce371bd9ba7bf6ed
Ilias <dougias.ilias@gmail.com>
2025-08-30 09:07:06 +0200

    refactor(vue_ls): Update vue and typescript configuration

--- lua/config/lsp/servers/vue_ts_ls.lua
+++ lua/config/lsp/servers/vue_ts_ls.lua
@@ -72,7 +72,41 @@ local inlayHints = {
     importModuleSpecifierPreference = 'non-relative',
 }
 
-vim.lsp.config.ts_ls = {
+local vue_language_server_path = vim.fn.expand('$MASON/packages')
+    .. '/vue-language-server'
+    .. '/node_modules/@vue/language-server'
+
+local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
+}
+
+local vue_ls_config = {}
+
+local vtsls_config = {
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                },
+            },
+        },
+    },
+    filetypes = tsserver_filetypes,
+}
+
+local ts_ls_config = {
+    init_options = {
+        plugins = {
+            vue_plugin,
+        },
+    },
+    filetypes = tsserver_filetypes,
     settings = {
         typescript = {
             inlayHints = inlayHints,
@@ -83,4 +117,8 @@ vim.lsp.config.ts_ls = {
     },
 }
 
-vim.lsp.enable('ts_ls')
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
+vim.lsp.config('ts_ls', ts_ls_config)
+
+vim.lsp.enable({ 'ts_ls', 'vue_ls' })
