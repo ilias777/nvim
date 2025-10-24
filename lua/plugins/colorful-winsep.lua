f12eafa8c33eb0ca47a43eddafa4b891d558f5fd
Ilias <dougias.ilias@gmail.com>
2025-10-24 20:35:32 +0200

    refactor(colorful-winsep): Change event

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-zh/colorful-winsep.nvim',
     enabled = true,
-    event = { 'WinLeave' },
+    event = { 'BufReadPre', 'BufNewFile' },
     opts = {
         indicator_for_2wins = {
             position = 'center', -- false | "center" | "start" | "end" | "both"
