96454d36f9e11e951cb1637c02a28bfa15f6ac62
Ilias <dougias.ilias@gmail.com>
2025-10-25 22:29:31 +0200

    feat(autocommand): Keep offset (scrolloff) in the end of the file

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -177,3 +177,20 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.keymap.set('n', '<CR>', '<CR>:cclose<CR>', { buffer = true, silent = true })
     end,
 })
+
+-- ╭─────────────────────────────────────────────────────────╮
+-- │    KEEP OFFSET IN THE END OF THE FILE IN INSERT MODE    │
+-- ╰─────────────────────────────────────────────────────────╯
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'BufEnter' }, {
+    callback = function()
+        local win_h = vim.api.nvim_win_get_height(0)
+        local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
+        local dist = vim.fn.line('$') - vim.fn.line('.')
+        local rem = vim.fn.line('w$') - vim.fn.line('w0') + 1
+        if dist < off and win_h - rem + dist < off then
+            local view = vim.fn.winsaveview()
+            view.topline = view.topline + off - (win_h - rem + dist)
+            vim.fn.winrestview(view)
+        end
+    end,
+})
