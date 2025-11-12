57abde1d5ab683c2fc626814bcf64f9a3af60a4d
Ilias <dougias.ilias@gmail.com>
2025-11-12 22:04:07 +0100

    feat(lint): Add rumdl linter for markdown

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -8,6 +8,27 @@ return {
             json = { 'jsonlint' },
             vue = { 'eslint' },
             yaml = { 'yamllint' },
+            markdown = { 'rumdl' },
+        }
+        lint.linters.rumdl = {
+            cmd = 'rumdl',
+            stdin = false, -- inconsistent if set to true
+            args = { 'check' },
+            stream = 'stdout',
+            ignore_exitcode = true,
+            env = nil,
+            parser = require('lint.parser').from_pattern('([^:]+):(%d+):(%d+): %[([^%]]+)%] (.+)', {
+                'file',
+                'lnum',
+                'col',
+                'code',
+                'message',
+            }, {
+                -- severity mapping -> useful if integrated with note taking tools eg markdown-oxide
+            }, {
+                source = 'rumdl',
+                severity = vim.diagnostic.severity.WARN,
+            }),
         }
         local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
         vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
