89ba70a551ae3d0d0d167bd1fd3cdec06ef3d0e0
Ilias <dougias.ilias@gmail.com>
2026-01-25 11:42:03 +0100

    fix(nvim-lint): Remove rumdl

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -8,7 +8,6 @@ return {
             json = { 'jsonlint' },
             vue = { 'eslint' },
             yaml = { 'yamllint' },
-            markdown = { 'rumdl' },
         }
         local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
         vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
