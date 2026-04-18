71e4e8b8bc86b010acfa88bb8e8198201929b2a2
Ilias <dougias.ilias@gmail.com>
2026-04-18 09:53:19 +0200

    fix: Update catppuccin colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,7 @@ return {
         priority = 1000,
         config = function()
             require('plugins.theme.catppuccin')
-            vim.cmd('colorscheme catppuccin')
+            vim.cmd('colorscheme catppuccin-nvim')
         end,
     },
 }
