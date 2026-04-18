6f4fd702704ef8f4b03e376790a1d23cdbaa1a87
Ilias <dougias.ilias@gmail.com>
2026-04-18 09:54:00 +0200

    refactor: Disable rainbow-delimeters.nvim

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,15 +1,12 @@
 return {
     'HiPhish/rainbow-delimiters.nvim',
-    enabled = true,
+    enabled = false,
     event = 'BufReadPre',
     config = function()
         local rainbow_delimiters = require('rainbow-delimiters')
-        local christmas = require('rainbow-delimiters.strategy.christmas')
-        local christmas_lights = christmas.lights(rainbow_delimiters.strategy['global'])
 
         vim.g.rainbow_delimiters = {
             strategy = {
-                -- [''] = christmas_lights,
                 [''] = rainbow_delimiters.strategy['global'],
                 vim = rainbow_delimiters.strategy['local'],
             },
