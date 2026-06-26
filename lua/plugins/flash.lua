f22829c949f37c7936c6f965f6beb3c823667352
Ilias <dougias.ilias@gmail.com>
2026-06-26 22:31:25 +0200

    feat(flash): Change configuration and add keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,8 +3,11 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
         label = {
-            ---@type "lowercase" | "all"
-            reuse = 'all',
+            uppercase = false,
+            rainbow = {
+                enabled = true,
+                shade = 5,
+            },
         },
         modes = {
             search = {
@@ -77,5 +80,20 @@ return {
             end,
             desc = 'Toggle Flash Search',
         },
+        vim.keymap.set('n', '<leader>s', function()
+            require('flash').jump({
+                pattern = '.',
+                label = { min_pattern_length = 2 },
+                search = {
+                    mode = function(pattern)
+                        if pattern:sub(1, 1) == '.' then
+                            pattern = pattern:sub(2)
+                        end
+                        return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                    end,
+                },
+                jump = { pos = 'range' },
+            })
+        end, { desc = 'Flash: jump to word' }),
     },
 }
